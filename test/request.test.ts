ccb73066b481a4e6e764d7614a9eb4797301b6d3
Andrey Meshkov <am@adguard.com>
2019-07-17 01:37:28 +0300

    Added request and tests for it

---
+++ test/request.test.ts
@@ -0,0 +1,51 @@
+import { RequestType, Request } from '../src/request';
+
+describe('Creating request', () => {
+    it('works if simple request is parsed properly', () => {
+        let request = new Request("http://example.org/", null, RequestType.Other);
+        expect(request.url).toEqual("http://example.org/");
+        expect(request.hostname).toEqual("example.org");
+        expect(request.domain).toEqual("example.org");
+        expect(request.sourceUrl).toBeNull();
+        expect(request.sourceHostname).toBeNull();
+        expect(request.sourceDomain).toBeNull();
+        expect(request.requestType).toEqual(RequestType.Other);
+        expect(request.thirdParty).toBeNull();
+    });
+
+    it('works if source URL is parsed properly', () => {
+        let request = new Request("http://example.org/", "http://sub.example.org", RequestType.Other);
+        expect(request.url).toEqual("http://example.org/");
+        expect(request.hostname).toEqual("example.org");
+        expect(request.domain).toEqual("example.org");
+        expect(request.sourceUrl).toEqual("http://sub.example.org");
+        expect(request.sourceHostname).toEqual("sub.example.org");
+        expect(request.sourceDomain).toEqual("example.org");
+        expect(request.requestType).toEqual(RequestType.Other);
+        expect(request.thirdParty).toEqual(false);
+    });
+
+    it('works if eTLD is parsed properly', () => {
+        let request = new Request("http://example.org.uk/", "http://sub.example.org.uk", RequestType.Other);
+        expect(request.url).toEqual("http://example.org.uk/");
+        expect(request.hostname).toEqual("example.org.uk");
+        expect(request.domain).toEqual("example.org.uk");
+        expect(request.sourceUrl).toEqual("http://sub.example.org.uk");
+        expect(request.sourceHostname).toEqual("sub.example.org.uk");
+        expect(request.sourceDomain).toEqual("example.org.uk");
+        expect(request.requestType).toEqual(RequestType.Other);
+        expect(request.thirdParty).toEqual(false);
+    });
+
+    it('works if third-party is detected properly', () => {
+        let request = new Request("http://example.org.uk/", "http://sub.example.org", RequestType.Other);
+        expect(request.url).toEqual("http://example.org.uk/");
+        expect(request.hostname).toEqual("example.org.uk");
+        expect(request.domain).toEqual("example.org.uk");
+        expect(request.sourceUrl).toEqual("http://sub.example.org");
+        expect(request.sourceHostname).toEqual("sub.example.org");
+        expect(request.sourceDomain).toEqual("example.org");
+        expect(request.requestType).toEqual(RequestType.Other);
+        expect(request.thirdParty).toEqual(true);
+    });
+});
