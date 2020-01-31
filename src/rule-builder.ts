9088e7e1715e69fe389fc0e60512e31a9d8636d6
mizzick <ds.kolyshev@gmail.com>
2020-01-31 23:30:28 +0700

    feature:AG-34 filter lists rule builder

---
+++ src/rule-builder.ts
@@ -0,0 +1,53 @@
+import { CosmeticRule } from './cosmetic-rule';
+import { NetworkRule } from './network-rule';
+import { IRule } from './rule';
+import { isCosmetic } from './cosmetic-rule-marker';
+
+/**
+ * Rule builder class
+ */
+export class RuleBuilder {
+    /**
+     * Creates rule of suitable class from text string
+     * It returns null if the line is empty or if it is a comment
+     *
+     * @param text rule string
+     * @param filterListId list id
+     * @return IRule object or null
+     */
+    public static createRule(text: string, filterListId: number): IRule | null {
+        if (!text || RuleBuilder.isComment(text)) {
+            return null;
+        }
+
+        const line = text.trim();
+
+        if (isCosmetic(line)) {
+            return new CosmeticRule(line, filterListId);
+        }
+
+        return new NetworkRule(line, filterListId);
+    }
+
+    /**
+     * If text is comment
+     *
+     * @param text
+     */
+    private static isComment(text: string): boolean {
+        if (text.charAt(0) === '!') {
+            return true;
+        }
+
+        if (text.charAt(0) === '#') {
+            if (text.length === 1) {
+                return true;
+            }
+
+            // Now we should check that this is not a cosmetic rule
+            return !isCosmetic(text);
+        }
+
+        return false;
+    }
+}
---
+++ test/rule-builder.test.ts
@@ -0,0 +1,32 @@
+import {RuleBuilder} from "../src/rule-builder";
+
+describe('Rule Builder Test', () => {
+    it('works if builder creates correct rules', () => {
+        let rule;
+
+        rule = RuleBuilder.createRule('', 1);
+        expect(rule).toBeFalsy();
+
+        rule = RuleBuilder.createRule('! comment', 1);
+        expect(rule).toBeFalsy();
+
+        rule = RuleBuilder.createRule('#', 1);
+        expect(rule).toBeFalsy();
+
+        rule = RuleBuilder.createRule('##.banner', 1);
+        expect(rule).toBeTruthy();
+        if (rule) {
+            expect(rule.getText()).toBe('##.banner');
+            expect(rule.getFilterListId()).toBe(1);
+            expect(rule.isCosmetic()).toBeTruthy();
+        }
+
+        rule = RuleBuilder.createRule('||example.org^', 1);
+        expect(rule).toBeTruthy();
+        if (rule) {
+            expect(rule.getText()).toBe('||example.org^');
+            expect(rule.getFilterListId()).toBe(1);
+            expect(rule.isCosmetic()).toBeFalsy();
+        }
+    });
+});
