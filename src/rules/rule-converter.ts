a0d88dc7dba1d0956f8ede760887caf5453687b5
mizzick <ds.kolyshev@gmail.com>
2020-05-14 21:44:57 +0200

    feature:AG-2084 rule converter

--- src/rules/rule-converter.ts
+++ src/rules/rule-converter.ts
@@ -5,6 +5,20 @@ import Scriptlets from 'scriptlets';
  */
 export class RuleConverter {
     /**
+     * Converts UBO and ABP redirect rules to AdGuard redirect rules
+     * @param rule
+     * @return {string} convertedRule
+     */
+    private static convertUboAndAbpRedirectsToAdg(rule: string): string | null {
+        const { redirects } = Scriptlets;
+        if (redirects.isUboRedirectCompatibleWithAdg(rule) || redirects.isAbpRedirectCompatibleWithAdg(rule)) {
+            return redirects.convertRedirectToAdg(rule);
+        }
+
+        return rule;
+    }
+
+    /**
      * Convert external scriptlet rule to AdGuard scriptlet syntax
      *
      * @param {string} rule convert rule
@@ -18,7 +32,12 @@ export class RuleConverter {
         // TODO: Convert ubo script rule
         // TODO: Convert ubo css rule
 
-        // TODO: Convert abp redirect rule
+        // Convert abp redirect rule
+        const abpRedirectRule = RuleConverter.convertUboAndAbpRedirectsToAdg(rule);
+        if (abpRedirectRule) {
+            return [abpRedirectRule];
+        }
+
         // TODO: Convert options
 
         return [rule];
