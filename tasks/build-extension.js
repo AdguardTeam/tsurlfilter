68b9e59c6be820a9b8bb9b1a1c332b5607997d8b
Maxim Topciu <maximtop@gmail.com>
2020-07-15 10:24:02 +0300

    AG-3153 rename AGUrlFilter to TSUrlFilter

--- tasks/build-extension.js
+++ tasks/build-extension.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra');
 
 const EXTENSION_SRC = './sample-extension/';
 const TSURLFILTER_DIST = './dist/tsurlfilter.es5.js';
-const TSURLFILTER_CONTENT_SCRIPT_DIST = './dist/AGUrlFilterContentScript.js';
+const TSURLFILTER_CONTENT_SCRIPT_DIST = './dist/TSUrlFilterContentScript.js';
 const EXTENSION_DIST = './dist-extension/';
 
 fs.copySync(EXTENSION_SRC, EXTENSION_DIST);
