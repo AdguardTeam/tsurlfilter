3ac73f22af240ae4821249a7399a7c135fdee0c0
Maxim Topciu <maximtop@gmail.com>
2020-08-14 17:58:01 +0300

    AG-3367 fix esm build

--- rollup.config.ts
+++ rollup.config.ts
@@ -37,9 +37,16 @@ export default [
         input: 'src/index.ts',
         output: [
             {
-                file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true,
+                file: pkg.main,
+                name: camelCase(libraryName),
+                format: 'umd',
+                sourcemap: true,
             },
-            { file: pkg.module, format: 'es', sourcemap: true },
+            {
+                file: pkg.module,
+                format: 'esm',
+                sourcemap: true,
+            },
         ],
         // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
         external: [],
