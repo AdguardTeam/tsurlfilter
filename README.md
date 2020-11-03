2af1896312e74b0f8fa550b6a5967e1f23146aae
mizzick <ds.kolyshev@gmail.com>
2020-11-03 23:24:04 +0200

    feature:AG-4560 performance parsing rules benchmark

--- README.md
+++ README.md
@@ -508,6 +508,7 @@ Test pages:
 -   `npm start`: Run `npm run build` in watch mode
 -   `npm run test:watch`: Run test suite in [interactive watch mode](https://jestjs.io/docs/en/cli#--watch)
 -   `npm run test:prod`: Run linting and generate coverage
+-   `npm run test:benchmarks`: Run benchmark tests, inspect in `chrome://inspect`
 -   `npm run build`: Generate bundles and typings, create docs
 -   `npm run lint`: Lints code
 -   `npm run commit`: Commit using conventional commit style ([husky](https://github.com/typicode/husky) will tell you to use it if you haven't :wink:)
---
+++ test/engine/start-engine-benchmark.test.ts
@@ -0,0 +1,47 @@
+import console from 'console';
+import fs from 'fs';
+import {
+    Engine, RuleStorage, setLogger, StringRuleList,
+} from '../../src';
+
+describe('Start Engine Benchmark', () => {
+    beforeAll(() => {
+        setLogger({
+            error(): void {
+            },
+            info(): void {
+            },
+            log(): void {
+            },
+            warn(): void {
+            },
+        });
+    });
+
+    afterAll(() => {
+        setLogger(console);
+    });
+
+    it('starts network-engine', async () => {
+        const rulesFilePath = './test/resources/adguard_base_filter.txt';
+
+        const ruleText = await fs.promises.readFile(rulesFilePath, 'utf8');
+
+        console.log('Starting engine..');
+        const startParse = Date.now();
+
+        let count = 0;
+        while (count < 10) {
+            count += 1;
+
+            const list = new StringRuleList(1, ruleText, false);
+            const ruleStorage = new RuleStorage([list]);
+
+            const engine = new Engine(ruleStorage, false);
+            expect(engine).toBeTruthy();
+            expect(engine.getRulesCount()).toEqual(91693);
+        }
+
+        console.log(`Elapsed on parsing rules: ${Date.now() - startParse}`);
+    });
+});
