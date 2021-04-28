f10b0691386c1cb02fa0431172832edb38e29f9e
Maxim Topciu <maximtop@gmail.com>
2021-04-28 10:43:08 +0300

    AG-7558 add increment and deploy plans

---
+++ tasks/build-txt.js
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires,@typescript-eslint/explicit-function-return-type */
+const fs = require('fs');
+const path = require('path');
+const { version } = require('../package.json');
+
+const PATH = '../dist';
+const FILENAME = 'build.txt';
+
+const main = () => {
+    const content = `version=${version}`;
+    const dir = path.resolve(__dirname, PATH);
+
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+
+    fs.writeFileSync(path.resolve(__dirname, PATH, FILENAME), content);
+};
+
+main();
