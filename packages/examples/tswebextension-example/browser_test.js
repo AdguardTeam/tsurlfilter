a42e9f32a1efac5c71528763ee8568ec5ed85dbc
Vladimir Zhelvis <zhelvisvladimir@gmail.com>
2021-11-18 15:15:28 +0300

    update browserr autotest tool

--- packages/examples/tswebextension-example/browser_test.js
+++ packages/examples/tswebextension-example/browser_test.js
@@ -1,74 +1,35 @@
 const path = require("path");
 const puppeteer = require("puppeteer");
+const axios = require("axios");
+
+
+const baseURL = 'https://testcases.adguard.com';
+
+axios.defaults.baseURL = baseURL;
 
 const pathToExtension = path.join(__dirname, "build");
 
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: [
-      `--disable-extensions-except=${pathToExtension}`,
-      `--load-extension=${pathToExtension}`,
-    ],
-  });
-  const targets = await browser.targets();
-  const backgroundPageTarget = targets.find(
-    (target) => target.type() === 'background_page'
-  );
-  const backgroundPage = await backgroundPageTarget.page();
 
-  await backgroundPage.waitForFunction('window.tsWebExtension?.isStarted', { polling: 100 });
+const testcases = [
+  {
+    rulesUrl: '/Filters/simple-rules/test-simple-rules.txt',
+    pageUrl: '/Filters/simple-rules/test-simple-rules.html'
+  },
+  {
+    rulesUrl: '/Filters/simple-rules/generichide-test/generichide-test.txt',
+    pageUrl: '/Filters/simple-rules/generichide-test/generichide-test.html'
+  }
+];
 
-  await backgroundPage.evaluate(async () => {
-    await window.tsWebExtension.configure({
-      filters: [{
-        filterId: 1,
-        content: `!
-          !
-          ! Title: Rules for simple rules tests
-          !
-          ! Filter to be used for testing purposes
-          ! https://testcases.adguard.com
-          !
-          ! Hide warning
-          testcases.adguard.com,surge.sh###subscribe-to-test-simple-rules-filter
-          ! Test case 1: domain-specific elemhide rule
-          testcases.adguard.com,surge.sh###case-1-elemhide > .test-banner
-          ! Test case 2: generic elemhide rule
-          ###case-2-generic-elemhide > .test-banner
-          ! Test case 3: elemhide rule exception
-          ###case-3-elemhide-exception > .test-banner
-          testcases.adguard.com,surge.sh#@##case-3-elemhide-exception > .test-banner
-          ! Test case 3: wildcard exception
-          testcases.adguard.com,surge.sh###case-3-elemhide-exception > h1
-          *#@##case-3-elemhide-exception > h1
-          ! Test case 3: generic exception
-          testcases.adguard.com,surge.sh###case-3-elemhide-exception > h2
-          #@##case-3-elemhide-exception > h2
-          ! Test case 3: generic exception for generic elemhide
-          ###case-3-elemhide-exception > h3
-          #@##case-3-elemhide-exception > h3
-          ! Test case 4: domain exclusion
-          ~testcases.adguard.com,~surge.sh###case-4-domain-exclusion > .test-banner
-          ! Test case 5: wildcard for tld
-          testcases.adguard.*,surge.*###case-5-wildcard-for-tld > .test-banner
-          ! Test case 6: wildcard for tld support with $domain modifier
-          ||*/tld-test-files/$image,domain=testcases.adguard.*|surge.*
-          ||*/tld*$script,domain=testcases.adguard.*|surge.*
-          ! Test case 7: $third-party modifier
-          ||antibanner.net^$third-party
-          ! Test case 8: $subdocument modifier
-          ||*/iframe-test-1.html^$subdocument,domain=testcases.adguard.com|surge.sh
-          ||*/iframe-test-2.html^$domain=testcases.adguard.com|surge.sh
-          @@||*/iframe-test-2.html^$subdocument,domain=testcases.adguard.com|surge.sh`
-      }],
-      allowlist: [],
-      userrules: [],
-      verbose: false,
-      settings: {
-        collectStats: true,
-        allowlistInverted: false,
-        stealth: {
+const defaultConfig = {
+  filters: [],
+  allowlist: [],
+  userrules: [],
+  verbose: false,
+  settings: {
+      collectStats: true,
+      allowlistInverted: false,
+      stealth: {
           blockChromeClientData: true,
           hideReferrer: true,
           hideSearchQueries: true,
@@ -78,14 +39,91 @@ const pathToExtension = path.join(__dirname, "build");
           selfDestructThirdPartyCookiesTime: 3600,
           selfDestructFirstPartyCookies: true,
           selfDestructFirstPartyCookiesTime: 3600,
-        },
       },
-    })
-  })
+  },
+};
+
+(async () => {
+  // Launch browser with installed extension
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      `--disable-extensions-except=${pathToExtension}`,
+      `--load-extension=${pathToExtension}`,
+    ],
+  });
+
+  const targets = await browser.targets();
+  const backgroundPageTarget = targets.find(
+    (target) => target.type() === 'background_page'
+  );
+  const backgroundPage = await backgroundPageTarget.page();
+
+
+  // Wait for tsWebExtension start
+  await backgroundPage.waitForFunction('window.tsWebExtension?.isStarted', { polling: 100 });
 
   const page = await browser.newPage();
 
-  await page.goto('https://testcases.adguard.com/Filters/simple-rules/test-simple-rules.html');
+  // register function, that transfer args from page to puppeteer context
+  // installed function survive navigations.
+  await page.exposeFunction('logDone', async (details) => {
+    console.log(details);
+  });
 
-  //await browser.close();
+  // extends QUnit instance on creation by custom event listeners,
+  // that triggers exposed function
+  await page.evaluateOnNewDocument(() => {
+    let qUnit;
+    Object.defineProperty(window, 'QUnit', {
+      get: () => qUnit,
+      set: (value) => {
+        qUnit = value;
+
+        qUnit.done(details => {
+          window.logDone(details)
+        })
+      },
+      configurable: true,
+    });
+  });
+
+  // run testcases
+  for (let testcase of testcases) {
+
+    let rulesText;
+
+    // load rules text for current testcase
+    try {
+      const res = await axios.get(testcase.rulesUrl, {
+        validateStatus: (status) => {
+          return status === 200; // Resolve only if the status code is 200
+        },
+      });
+      
+      rulesText = res.data;
+    } catch (e) {
+      console.log(e.mesage);
+    }
+
+    if(!rulesText){
+      continue;
+    }
+
+    // update tsWebExtension config
+    await backgroundPage.evaluate(async (defaultConfig, rulesText) => {
+      await window.tsWebExtension.configure({
+        ...defaultConfig,
+        filters: [{
+          filterId: 1,
+          content: rulesText
+        }],
+      })
+    }, defaultConfig, rulesText);
+
+    // run test page
+    await page.goto(`${baseURL}${testcase.pageUrl}`, { waitUntil: 'networkidle0'});
+  }
+
+  await browser.close();
 })();
