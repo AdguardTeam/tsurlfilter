35082d2c34cee80b8c93a0652636f3cf80cfcc01
Slava Leleka <v.leleka@adguard.com>
2023-06-19 15:47:32 +0300

    AG-23002 update modifiers yaml files
    Squashed commit of the following:
    
    commit 40b9e339f6397b79ee798db241556489c08b0eef
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Mon Jun 19 15:23:15 2023 +0300
    
        fix linter
    
    commit 9e3a240dd60e4ababb9c3b92b9c58b01ed355a53
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Mon Jun 19 15:17:22 2023 +0300
    
        beautify modifier yaml files
    
    commit 41ba2a9646090e2fe7b192dd21168d46e75a27b7
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Mon Jun 19 15:14:42 2023 +0300
    
        update src/compatibility-tables/modifiers
    
    commit 2c3195eb16121c790a358ba7d348e4ac3e708a86
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Thu Jun 15 22:47:45 2023 +0300
    
        beautify modifier yaml files

---
+++ packages/agtree/src/compatibility-tables/modifiers.ts
@@ -0,0 +1,92 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
+import app from './modifiers/app.yml';
+import badfilter from './modifiers/badfilter.yml';
+import cname from './modifiers/cname.yml';
+import content from './modifiers/content.yml';
+import cookie from './modifiers/cookie.yml';
+import csp from './modifiers/csp.yml';
+import denyallow from './modifiers/denyallow.yml';
+import document from './modifiers/document.yml';
+import domain from './modifiers/domain.yml';
+import elemhide from './modifiers/elemhide.yml';
+import extension from './modifiers/extension.yml';
+import font from './modifiers/font.yml';
+import genericblock from './modifiers/genericblock.yml';
+import generichide from './modifiers/generichide.yml';
+import header from './modifiers/header.yml';
+import hls from './modifiers/hls.yml';
+import image from './modifiers/image.yml';
+import important from './modifiers/important.yml';
+import inlineFont from './modifiers/inline-font.yml';
+import jsinject from './modifiers/jsinject.yml';
+import jsonprune from './modifiers/jsonprune.yml';
+import matchcase from './modifiers/matchcase.yml';
+import media from './modifiers/media.yml';
+import object from './modifiers/object.yml';
+import other from './modifiers/other.yml';
+import ping from './modifiers/ping.yml';
+import popunder from './modifiers/popunder.yml';
+import popup from './modifiers/popup.yml';
+import redirectRule from './modifiers/redirect-rule.yml';
+import removeheader from './modifiers/removeheader.yml';
+import script from './modifiers/script.yml';
+import stealth from './modifiers/stealth.yml';
+import strict1p from './modifiers/strict1p.yml';
+import strict3p from './modifiers/strict3p.yml';
+import stylesheet from './modifiers/stylesheet.yml';
+import subdocument from './modifiers/subdocument.yml';
+import thirdParty from './modifiers/third-party.yml';
+import to from './modifiers/to.yml';
+import urlblock from './modifiers/urlblock.yml';
+import webrtc from './modifiers/webrtc.yml';
+import websocket from './modifiers/websocket.yml';
+import xmlhttprequest from './modifiers/xmlhttprequest.yml';
+
+import { type RawModifierData } from '.';
+
+export const rawModifiersData: RawModifierData = {
+    app,
+    badfilter,
+    cname,
+    content,
+    cookie,
+    csp,
+    denyallow,
+    document,
+    domain,
+    elemhide,
+    extension,
+    font,
+    genericblock,
+    generichide,
+    header,
+    hls,
+    image,
+    important,
+    inlineFont,
+    jsinject,
+    jsonprune,
+    matchcase,
+    media,
+    object,
+    other,
+    ping,
+    popunder,
+    popup,
+    redirectRule,
+    removeheader,
+    script,
+    stealth,
+    strict1p,
+    strict3p,
+    stylesheet,
+    subdocument,
+    thirdParty,
+    to,
+    urlblock,
+    webrtc,
+    websocket,
+    xmlhttprequest,
+};
