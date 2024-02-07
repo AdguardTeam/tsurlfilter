b93929b9151da8be9a58b71ced451cfca347d963
scripthunter7 <d.tota@adguard.com>
2024-02-07 13:49:48 +0100

    small demo file

---
+++ packages/agtree/src/utils/encoder-demo.ts
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+// FIXME: temporary file
+// use: npx tsx encoder-demo.ts
+import { ByteBuffer } from './byte-buffer-new';
+import { decode } from './text-decoder';
+import { encode } from './text-encoder';
+
+const buffer = new ByteBuffer();
+const str = '你好';
+const byteOffsetBefore = buffer.byteOffset;
+const bytesWritten = encode(str, buffer);
+console.log(`Bytes written: ${bytesWritten}`);
+console.log(`Buffer: ${buffer.subarray(byteOffsetBefore, byteOffsetBefore + bytesWritten)}`);
+const decodedStr = decode(buffer, byteOffsetBefore, bytesWritten);
+console.log(`Decoded string: ${decodedStr}`);
+console.log(`Byte offset: ${buffer.byteOffset}`);
