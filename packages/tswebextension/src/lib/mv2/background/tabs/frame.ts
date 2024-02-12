0b1862eafd6391cf79809b20c22ee0665b417c5c
Vladimir Zhelvis <v.zhelvis@adguard.com>
2024-02-12 12:19:09 +0300

    Pull request #797: AG-30206 Check possible leaks of memory
    Merge in ADGUARD-FILTERS/tsurlfilter from fix/AG-30206 to master
    
    Squashed commit of the following:
    
    commit e2950e9816ee38bdabeacca9a6672efd253b009b
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Mon Feb 12 12:09:43 2024 +0300
    
        rename RequestContextStorage.stopCleanup to RequestContextStorage.clearCleanupTimer
    
    commit e447cbf87299033711774d6bee5f1b49cdca3c28
    Author: Andrey Meshkov <am@adguard.com>
    Date:   Mon Feb 12 11:37:16 2024 +0300
    
        packages/tswebextension/src/lib/mv2/background/request/request-context-storage.ts edited online with Bitbucket
    
    commit 2332008560d2890c287a645618aca3148524d6f0
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Mon Feb 12 11:35:06 2024 +0300
    
        delete FIXME
    
    commit 7892724634bf87acc21a51c1006049a098318aed
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Mon Feb 12 11:17:15 2024 +0300
    
        add logger.setVerbose
    
    commit d8a3619b092c0d408ede9554a5cdfc372ec2b713
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Sat Feb 10 17:22:32 2024 +0300
    
        update logger
    
    commit 0f1851a9957221199e22c104a3c3ca049ac60ad7
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Fri Feb 9 20:22:37 2024 +0300
    
        update scheduleCleanup desc
    
    commit 611ad8d7f175897cc5a4cc79368eede5e3199a4c
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Fri Feb 9 20:19:27 2024 +0300
    
        update request context timestamp desc
    
    commit f225850face8619ca59508f207a99cce0db1cc38
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Fri Feb 9 20:17:36 2024 +0300
    
        update request context storage
    
    commit d1601af9861a1bd060bc3511c7b6a203deadbb8d
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Fri Feb 9 18:31:24 2024 +0300
    
        add description for frame context class
    
    commit 6d482fa85ad4ef981344f2175efb1fd4803c8a14
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Fri Feb 9 17:41:10 2024 +0300
    
        small fixes
    
    commit a29a76dbd9d08290577a154833f557a5302d7b65
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Fri Feb 9 17:27:39 2024 +0300
    
        update request context storage
    
    commit 76b9e67b05049bc793399b5356d97ae674aec4bf
    Merge: 2ee4f15b a8fbb0e1
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Fri Feb 9 13:04:45 2024 +0300
    
        Merge branch 'master' into fix/AG-30206
    
    commit 2ee4f15b2feab52377b13f50962a7e72d93663ac
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Fri Feb 9 12:09:05 2024 +0300
    
        do not overwrite deleted request context record

--- packages/tswebextension/src/lib/mv2/background/tabs/frame.ts
+++ packages/tswebextension/src/lib/mv2/background/tabs/frame.ts
@@ -7,6 +7,10 @@ export const MAIN_FRAME_ID = 0;
 
 /**
  * Frame context data.
+ * We store {@link MatchingResult} and {@link CosmeticResult} in the frame context
+ * to apply rules that cannot be handled during request processing.
+ * The frame data is deleted after the {@link browser.webNavigation.onCompleted} event.
+ * @see {@link WebRequestApi.deleteFrameContext}
  */
 export class Frame {
     /**
@@ -21,15 +25,17 @@ export class Frame {
 
     /**
      * Frame cosmetic result.
-     * This data is saved in frame, because we need for access it
-     * after request content data delete.
+     * This data is saved in the frame because we need to access it for css injection
+     * after deleting request context data.
+     *
+     * @see {@link WebRequestApi.injectCosmetic}
      */
     public cosmeticResult?: CosmeticResult;
 
     /**
      * Frame matching result.
-     * This data is saved in frame, because we need for access it
-     * after request content data delete.
+     * This data is saved in frame, because we need for access it for script rules injection
+     * after deleting request context data.
      */
     public matchingResult?: MatchingResult | null;
 
