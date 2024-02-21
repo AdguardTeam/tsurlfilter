6aecf97fc28c190a94947ebe4dd46e9f87aab33a
scripthunter7 <d.tota@adguard.com>
2024-02-21 10:48:12 +0100

    add known scriptlets

--- packages/agtree/src/parser/cosmetic/body/abp-snippet.ts
+++ packages/agtree/src/parser/cosmetic/body/abp-snippet.ts
@@ -43,8 +43,35 @@ export class AbpSnippetInjectionBodyParser extends ParserBase {
      * @note Only 256 values can be represented this way.
      */
     private static readonly FREQUENT_ARGS_SERIALIZATION_MAP = new Map<string, number>([
-        // FIXME: Add frequently used scriptlet names and their serialization index
-        // ['scriptlet-name', 0],
+        ['abort-current-inline-script', 0],
+        ['abort-on-property-read', 1],
+        ['abort-on-property-write', 2],
+        ['json-prune', 3],
+        ['log', 4],
+        ['prevent-listener', 5],
+        ['cookie-remover', 6],
+        ['override-property-read', 7],
+        ['abort-on-iframe-property-read', 8],
+        ['abort-on-iframe-property-write', 9],
+        ['freeze-element', 10],
+        ['json-override', 11],
+        ['simulate-mouse-event', 12],
+        ['strip-fetch-query-parameter', 13],
+        ['hide-if-contains', 14],
+        ['hide-if-contains-image', 15],
+        ['hide-if-contains-image-hash', 16],
+        ['hide-if-contains-similar-text', 17],
+        ['hide-if-contains-visible-text', 18],
+        ['hide-if-contains-and-matches-style', 19],
+        ['hide-if-graph-matches', 20],
+        ['hide-if-has-and-matches-style', 21],
+        ['hide-if-labelled-by', 22],
+        ['hide-if-matches-xpath', 23],
+        ['hide-if-matches-computed-xpath', 24],
+        ['hide-if-shadow-contains', 25],
+        ['debug', 26],
+        ['trace', 27],
+        ['race', 28],
     ]);
 
     /**
