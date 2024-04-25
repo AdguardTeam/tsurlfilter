455b3e924840b5e47506986faf56424658ae2f0b
Vladimir Zhelvis <v.zhelvis@adguard.com>
2024-04-25 17:34:40 +0300

    add missed binary map benchmark tests

---
+++ packages/tsurlfilter/test/utils/binary-map.test.ts
@@ -0,0 +1,85 @@
+import { BinaryMap } from '../../src/utils/binary-map';
+import { ByteBuffer } from '../../src/utils/byte-buffer';
+
+/**
+ * Last benchmark result:
+ *
+ * Env: MacOS, i7, 16GB ram.
+ * 10000 rounds.
+ * ┌──────────────┬────────────┬─────────────────┐
+ * │ dataset size │   map (ms) │ binary map (ms) │
+ * ├──────────────┼────────────┼─────────────────┤
+ * │ 100          │ 0.000244   │  0.0005         │
+ * │ 1000         │ 0.00022    │  0.000301       │
+ * │ 10000        │ 0.000263   │  0.000406       │
+ * │ 100000       │ 0.000444   │  0.000807       │
+ * └──────────────┴────────────┴─────────────────┘
+ */
+describe('Performance benchmark', () => {
+    const ROUNDS = 10_000;
+
+    const MIN_SIZE_EXPONENT = 2;
+    const MAX_SIZE_EXPONENT = 5;
+
+    /**
+     * Generates a random number between 0 and the specified maximum value.
+     * @param max The maximum value for the random number.
+     * @returns A random number between 0 and the specified maximum value.
+     */
+    function getRandomNumber(max: number): number {
+        // `| 0` is more compact and faster than `Math.floor()`.
+        return (Math.random() * max) | 0;
+    }
+
+    /**
+     * Gets the average value for the specified array of numbers.
+     *
+     * @param array - The array of numbers for which to calculate the average value.
+     * @returns The average value of the numbers in the array.
+     */
+    function getAverageValue(array: number[]): number {
+        return Number((array.reduce((a, b) => a + b, 0) / array.length).toFixed(6));
+    }
+
+    it('should be fast', () => {
+        const res: { [size: number]: { [structure: string]: number } } = {};
+
+        for (let i = MIN_SIZE_EXPONENT; i <= MAX_SIZE_EXPONENT; i += 1) {
+            const size = 10 ** i;
+
+            const buffer = new ByteBuffer();
+            const map = new Map<number, number>();
+
+            for (let j = 0; j < size; j += 1) {
+                map.set(j, j);
+            }
+
+            const binaryMap = BinaryMap.create(map, buffer);
+
+            const mapMeasurements: number[] = [];
+            const binaryMapMeasurements: number[] = [];
+
+            for (let k = 0; k < ROUNDS; k += 1) {
+                const key = getRandomNumber(size);
+
+                let start = performance.now();
+                map.get(key);
+                mapMeasurements.push(performance.now() - start);
+
+                start = performance.now();
+                BinaryMap.get(key, buffer, binaryMap);
+                binaryMapMeasurements.push(performance.now() - start);
+            }
+
+            res[size] = {
+                map: getAverageValue(mapMeasurements),
+                'binary map': getAverageValue(binaryMapMeasurements),
+            };
+        }
+
+        // eslint-disable-next-line no-console
+        console.table(res);
+
+        expect(true).toBe(true);
+    });
+});
