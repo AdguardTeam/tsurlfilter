b8e89a388f8bed8000304955d68f92337fa5a2ee
Vladimir Zhelvis <v.zhelvis@adguard.com>
2024-05-14 16:41:26 +0300

    add start test for dns engine

--- packages/tsurlfilter/test/engine/start-engine-benchmark.test.ts
+++ packages/tsurlfilter/test/engine/start-engine-benchmark.test.ts
@@ -1,11 +1,12 @@
 import console from 'console';
 import fs from 'fs';
 import {
+    BufferRuleList,
+    DnsEngine,
     Engine,
     NetworkEngine,
     RuleStorage,
     setLogger,
-    StringRuleList,
 } from '../../src';
 import { ByteBuffer } from '../../src/utils/byte-buffer';
 
@@ -57,7 +58,7 @@ describe('Start Engine Benchmark', () => {
         while (count < loopsCount) {
             count += 1;
 
-            const list = new StringRuleList(1, ruleText, false);
+            const list = new BufferRuleList(1, ruleText, false);
             const ruleStorage = new RuleStorage([list]);
 
             buffer = new ByteBuffer();
@@ -116,7 +117,7 @@ describe('Start Engine Benchmark', () => {
         while (count < loopsCount) {
             count += 1;
 
-            const list = new StringRuleList(1, ruleText, false);
+            const list = new BufferRuleList(1, ruleText, false);
             const ruleStorage = new RuleStorage([list]);
 
             buffer = new ByteBuffer();
@@ -135,6 +136,68 @@ describe('Start Engine Benchmark', () => {
 
             expect(networkEngineFromBuffer).toBeTruthy();
             expect(networkEngineFromBuffer.rulesCount).toEqual(44613);
+        }
+
+        const coldAverage = coldMeasurements.reduce((a, b) => a + b, 0) / loopsCount;
+        const hotAverage = hotMeasurements.reduce((a, b) => a + b, 0) / loopsCount;
+        const differenceInPercent = ((hotAverage - coldAverage) / coldAverage) * 100;
+        console.table({
+            cold: `${toFixed(coldAverage)} ms`,
+            hot: `${toFixed(hotAverage)} ms`,
+            diff: `${toFixed(differenceInPercent)} %`,
+        });
+
+        console.log(`Elapsed on parsing rules: ${toFixed(performance.now() - startParse)} ms`);
+    });
+
+    it('starts dns engine', async () => {
+        const rulesFilePath = './test/resources/adguard_sdn_filter.txt';
+        const hostsFilePath = './test/resources/hosts';
+
+        const rulesText = await fs.promises.readFile(rulesFilePath, 'utf8');
+        const hostsText = await fs.promises.readFile(hostsFilePath, 'utf8');
+
+        console.log('Starting dns engine...');
+        const startParse = performance.now();
+
+        // Starting dns engine...
+        // ┌─────────┬─────────────┐
+        // │ (index) │   Values    │
+        // ├─────────┼─────────────┤
+        // │  cold   │ '191.61 ms' │
+        // │   hot   │  '0.76 ms'  │
+        // │  diff   │ '-99.61 %'  │
+        // └─────────┴─────────────┘
+        // Elapsed on parsing rules: 1979.51 ms
+
+        let count = 0;
+        let buffer;
+        const loopsCount = 10;
+        const coldMeasurements = [];
+        const hotMeasurements = [];
+        while (count < loopsCount) {
+            count += 1;
+
+            const ruleList = new BufferRuleList(1, rulesText, false);
+            const hostList = new BufferRuleList(2, hostsText, false);
+            const ruleStorage = new RuleStorage([ruleList, hostList]);
+
+            buffer = new ByteBuffer();
+            const coldStart = performance.now();
+            const dnsEngine = DnsEngine.create(ruleStorage, buffer, false);
+            coldMeasurements.push(performance.now() - coldStart);
+
+            expect(dnsEngine).toBeTruthy();
+            expect(dnsEngine.rulesCount).toEqual(55999);
+
+            const newBuffer = new ByteBuffer(buffer.data); // copy buffer
+
+            const hotStart = performance.now();
+            const dnsEngineFromBuffer = DnsEngine.from(ruleStorage, newBuffer);
+            hotMeasurements.push(performance.now() - hotStart);
+
+            expect(dnsEngineFromBuffer).toBeTruthy();
+            expect(dnsEngineFromBuffer.rulesCount).toEqual(55999);
         }
 
         const coldAverage = coldMeasurements.reduce((a, b) => a + b, 0) / loopsCount;
