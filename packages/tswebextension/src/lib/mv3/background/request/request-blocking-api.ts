86e6226a5466b4dd9cc16940a4761c5c53db3169
Dmitry Seregin <d.seregin@adguard.com>
2024-05-15 17:28:26 +0300

    AG-32248: added tabs context
    Merge in ADGUARD-FILTERS/tsurlfilter from feature/AG-24586 to release/v2.3
    
    Squashed commit of the following:
    
    commit 62eadb29682d5384071a901b8022403f765a6736
    Merge: 0b92f9b8 6c2a586c
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Wed May 15 17:20:34 2024 +0300
    
        Merge branch 'release/v2.3' into feature/AG-24586
    
    commit 0b92f9b85bac437f462c5fd269139293e5cdfb09
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Tue May 14 18:44:12 2024 +0300
    
        add comment
    
    commit 278b1b4d96dc5542fe8e3bec80d559cf09f5679d
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Tue May 14 18:38:30 2024 +0300
    
        small fix
    
    commit 8a384fffd8e26e60f01d613242523012870d8bad
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Tue May 14 14:23:54 2024 +0300
    
        fixed linter errors
    
    commit 2404b69b55bc8fcc641763af2f9b51def5862762
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Tue May 14 14:23:47 2024 +0300
    
        fixed linter errors
    
    commit b2a5a059dc95072d07f1204c03ec0cc6c4c3fb16
    Merge: 664645d2 a87f769e
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Tue May 14 14:14:24 2024 +0300
    
        Merge branch 'release/v2.3' into feature/AG-24586
    
    commit 664645d29a538c232753196321e4b37ad37c7aa1
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Tue May 14 14:10:44 2024 +0300
    
        fixed docs
    
    commit fbeaa5853e3fb70e77bb7504c4a7239a06348ae5
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Mon May 13 16:33:55 2024 +0300
    
        added default filtering log usage with basic rule case
    
    commit 087423321daa5878f45279bd2d6dd96b15f5c056
    Merge: e8d26f4d 80258d09
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Mon May 13 14:22:08 2024 +0300
    
        Merge branch 'release/v2.3' into feature/AG-24586
    
    commit e8d26f4d28b0ccf2cdfd89f9ba2bc6c7937c3b89
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Mon May 13 14:07:19 2024 +0300
    
        added tests && fixes
    
    commit a8718f2b7f02ab703173a85c8a61e7729db4dd54
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Thu Apr 25 18:19:20 2024 +0300
    
        correct export
    
    commit 2829cc2f71a217b704d34ae94c325bebdf29f9e6
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Wed Apr 24 18:00:57 2024 +0300
    
        AG-32248: added tabs context

---
+++ packages/tswebextension/src/lib/mv3/background/request/request-blocking-api.ts
@@ -0,0 +1,176 @@
+import browser, { type WebRequest } from 'webextension-polyfill';
+import {
+    RequestType,
+    NetworkRuleOption,
+    type NetworkRule,
+} from '@adguard/tsurlfilter';
+
+import { tabsApi } from '../../tabs/tabs-api';
+import { FilteringEventType, defaultFilteringLog } from '../../../common/filtering-log';
+import { ContentType } from '..';
+
+/**
+ * Base params about request.
+ */
+type RequestParams = {
+    tabId: number,
+    referrerUrl: string,
+    requestUrl: string,
+    requestType: RequestType,
+};
+
+/**
+ * Params for {@link RequestBlockingApi.getBlockingResponse}.
+ */
+export type GetBlockingResponseParams = RequestParams & {
+    rule: NetworkRule | null,
+    popupRule: NetworkRule | null,
+};
+
+/**
+ * Params for {@link RequestBlockingApi.getResponseOnHeadersReceived}.
+ */
+export type GetHeadersResponseParams = RequestParams & {
+    rule: NetworkRule | null,
+};
+
+/**
+ * Api for processing request filtering.
+ *
+ * Method {@link getBlockingResponse} processes rule applying for request and computes response
+ * for {@link WebRequestApi.onBeforeRequest} listener.
+ *
+ * Method {@link shouldCollapseElement} checks, if initializer for request should be collapsed by content-script.
+ *
+ * This class also provides method {@link isRequestBlockedByRule} for checking, if rule is blocking rule.
+ */
+export class RequestBlockingApi {
+    /**
+     * Closes the tab which considered as a popup.
+     *
+     * @param data Needed data for logging closing of tab.
+     * @param appliedRule Network rule which was applied to request. This field
+     * is needed because data contains two rules: one for the request and
+     * one for the popup. And we should log only the rule which was applied
+     * to the request.
+     *
+     * @returns Response for {@link RequestApi.onBeforeRequest} listener.
+     */
+    private static closeTab(
+        data: RequestParams,
+        appliedRule: NetworkRule | null,
+    ): WebRequest.BlockingResponse {
+        RequestBlockingApi.logRuleApplying(data, appliedRule);
+        browser.tabs.remove(data.tabId);
+
+        return { cancel: true };
+    }
+
+    /**
+     * Processes rule applying for request and compute response for {@link WebRequestApi.onBeforeRequest} listener.
+     *
+     * @param data Data for request processing.
+     *
+     * @returns Response for {@link WebRequestApi.onBeforeRequest} listener.
+     */
+    public static getBlockingResponse(data: GetBlockingResponseParams): WebRequest.BlockingResponse | void {
+        const {
+            rule,
+            popupRule,
+            requestType,
+            tabId,
+        } = data;
+
+        if (!rule) {
+            return undefined;
+        }
+
+        // popup rule will be handled in the condition with requestType === document below
+        if (popupRule === rule && requestType !== RequestType.Document) {
+            return undefined;
+        }
+
+        if (rule.isAllowlist()) {
+            RequestBlockingApi.logRuleApplying(data, rule);
+            return undefined;
+        }
+
+        if (rule.isOptionEnabled(NetworkRuleOption.Redirect)) {
+            // TODO: Check that redirected url exists in our resources as in mv2.
+            return { redirectUrl: '' };
+        }
+
+        // Basic rules for blocking requests are applied only to sub-requests
+        // so `||example.com^` will not block the main page
+        // https://adguard.com/kb/general/ad-filtering/create-own-filters/#basic-rules
+        // For document requests we need to show blocking page or close tab.
+        if (requestType === RequestType.Document) {
+            // Blocking rule can be with $popup modifier - in this case we need
+            // to close the tab as soon as possible.
+            // https://adguard.com/kb/general/ad-filtering/create-own-filters/#popup-modifier
+            if (popupRule && tabsApi.isNewPopupTab(tabId)) {
+                return RequestBlockingApi.closeTab(data, popupRule);
+            }
+            // to handle rules with $all modifier, where popup was added implicitly
+            if (rule.isOptionEnabled(NetworkRuleOption.Popup) && tabsApi.isNewPopupTab(tabId)) {
+                return RequestBlockingApi.closeTab(data, rule);
+            }
+
+            // we do not want to block the main page if rule has only $popup modifier
+            if (rule === popupRule && !tabsApi.isNewPopupTab(tabId)) {
+                return undefined;
+            }
+
+            // but if the blocking rule has $document modifier, blocking page should be shown
+            // e.g. `||example.com^$document`
+            if ((rule.getPermittedRequestTypes() & RequestType.Document) === RequestType.Document) {
+                RequestBlockingApi.logRuleApplying(data, rule);
+                return { cancel: true };
+            }
+
+            return undefined;
+        }
+
+        RequestBlockingApi.logRuleApplying(data, rule);
+        return { cancel: true };
+    }
+
+    /**
+     * Creates {@link FilteringLog} event of rule applying for processed request.
+     *
+     * @param data Data for request processing.
+     * @param appliedRule Network rule which was applied to request.
+     */
+    private static logRuleApplying(
+        data: RequestParams,
+        appliedRule: NetworkRule | null,
+    ): void {
+        const {
+            tabId,
+            referrerUrl,
+            requestUrl,
+            requestType,
+        } = data;
+
+        if (!appliedRule || requestType === 0) {
+            return;
+        }
+
+        // We need this only for count total blocked requests,
+        // so we can skip contentType.
+        defaultFilteringLog.publishEvent({
+            type: FilteringEventType.ApplyBasicRule,
+            data: {
+                tabId,
+                // TODO: Check if eventId is needed in mv3.
+                eventId: '1',
+                // TODO: Add saving correct request type to request context
+                // storage in the upper level.
+                requestType: ContentType.Document,
+                frameUrl: referrerUrl,
+                requestUrl,
+                rule: appliedRule,
+            },
+        });
+    }
+}
---
+++ packages/tswebextension/src/lib/mv3/tabs/tab-context.ts
@@ -0,0 +1,307 @@
+import browser from 'webextension-polyfill';
+import { RequestType } from '@adguard/tsurlfilter/es/request-type';
+import type { CosmeticResult, MatchingResult, NetworkRule } from '@adguard/tsurlfilter';
+import type { Tabs } from 'webextension-polyfill';
+
+import { Frame, MAIN_FRAME_ID } from './frame';
+import {
+    type FilteringLog,
+    defaultFilteringLog,
+    FilteringEventType,
+} from '../../common/filtering-log';
+import { engineApi } from '../background/engine-api';
+import { isHttpOrWsRequest } from '../../common/utils/url';
+
+/**
+ * We need tab id in the tab information, otherwise we do not process it.
+ * For example developer tools tabs.
+ */
+export type TabInfo = Tabs.Tab & {
+    id: number,
+};
+
+/**
+ * Request context data related to the frame.
+ */
+export type FrameRequestContext = {
+    frameId: number;
+    requestId: string;
+    requestUrl: string;
+    requestType: RequestType;
+    isRemoveparamRedirect?: boolean;
+};
+
+/**
+ * Tab context.
+ */
+export class TabContext {
+    /**
+     * Frames context.
+     *
+     * NOTE: this is temporary storage for frames data.
+     * Each frame context is deleted after navigation is complete.
+     * Storage is cleared on tab reload.
+     * Do not use it as a data source out of request or navigation processing.
+     */
+    public frames = new Map<number, Frame>();
+
+    /**
+     * Blocked request count.
+     */
+    public blockedRequestCount = 0;
+
+    /**
+     * Document level rule, applied to the tab.
+     */
+    public mainFrameRule: NetworkRule | null = null;
+
+    /**
+     * We mark these tabs as synthetic because they may not actually exist.
+     */
+    public isSyntheticTab = true;
+
+    /**
+     * Is document page request handled by memory cache or sw.
+     */
+    public isDocumentRequestCached = false;
+
+    /**
+     * Timestamp of the assistant initialization.
+     *
+     * Needed to avoid cosmetic rules injection into the assistant frame.
+     *
+     * @see {@link https://github.com/AdguardTeam/AdguardBrowserExtension/issues/1848}
+     */
+    public assistantInitTimestamp?: number | null = null;
+
+    /**
+     * Context constructor.
+     *
+     * @param info Webextension API tab data.
+     * @param filteringLog Filtering Log API.
+     */
+    constructor(
+        public info: TabInfo,
+        private readonly filteringLog: FilteringLog = defaultFilteringLog,
+    ) {
+        this.info = info;
+    }
+
+    /**
+     * Updates tab info.
+     *
+     * @param changeInfo Tab change info.
+     * @param tabInfo Tab info.
+     */
+    public updateTabInfo(changeInfo: Tabs.OnUpdatedChangeInfoType, tabInfo: TabInfo): void {
+        this.info = tabInfo;
+
+        // If the tab was updated it means that it wasn't used to send requests in the background.
+        this.isSyntheticTab = false;
+
+        // Update main frame data when we navigate to another page with document request caching enabled.
+        if (changeInfo.url) {
+            // Get current main frame.
+            const frame = this.frames.get(MAIN_FRAME_ID);
+
+            // If main frame url is the same as request url, do nothing.
+            if (frame?.url === changeInfo.url) {
+                return;
+            }
+
+            // If the main frame doesn't exist or its URL is different from the request URL,
+            // it means that the document request hasn't been processed by the WebRequestApi yet.
+            // In this case, we mark the tab as using the cache and update its context using the tabsApi.
+            this.isDocumentRequestCached = true;
+
+            // Update main frame data.
+            this.handleMainFrameRequest(changeInfo.url);
+        }
+
+        // When the cached page is reloaded, we need to manually update
+        // the main frame rule for correct document-level rule processing.
+        if (!changeInfo.url
+            && changeInfo.status === 'loading'
+            && this.isDocumentRequestCached
+            && this.info.url) {
+            this.handleMainFrameRequest(this.info.url);
+        }
+    }
+
+    /**
+     * Updates main frame data.
+     *
+     * Note: this method will be called on tab reload before the first {@link browser.tabs.onUpdated} event
+     * and {@link handleTabUpdate} and {@link updateTabInfo} calls.
+     *
+     * @param tabId Tab ID.
+     * @param url Url.
+     */
+    public updateMainFrameData(tabId: number, url: string): void {
+        this.info.url = url;
+        this.info.id = tabId;
+
+        // Get current main frame.
+        const frame = this.frames.get(MAIN_FRAME_ID);
+
+        // If main frame url is the same as request url, do nothing.
+        if (frame?.url === url) {
+            return;
+        }
+
+        /**
+         * If the main frame doesn't exist or its URL is different from the request URL,
+         * we mark the tab as using the cache and update its context using the tabsApi,
+         * as it means that the document request hasn't been processed by the WebRequestApi yet.
+         */
+        this.isDocumentRequestCached = true;
+
+        // Update main frame data.
+        this.handleMainFrameRequest(url);
+    }
+
+    /**
+     * Increments blocked requests count.
+     */
+    public incrementBlockedRequestCount(): void {
+        this.blockedRequestCount += 1;
+    }
+
+    /**
+     * Handles document or subdocument request and stores data in specified frame context.
+     * If the request is a document request, will also match the main frame rule
+     * and store it in the {@link mainFrameRule} property.
+     * This method is called before filtering processing in WebRequest onBeforeRequest handler.
+     * MatchingResult is handled in {@link handleFrameMatchingResult}.
+     *
+     * CosmeticResult is handled in {@link handleFrameCosmeticResult}.
+     *
+     * @param requestContext Request context data.
+     * @param isRemoveparamRedirect Indicates whether the request is a $removeparam redirect.
+     */
+    public handleFrameRequest(requestContext: FrameRequestContext, isRemoveparamRedirect = false): void {
+        // This method is called in the WebRequest onBeforeRequest handler.
+        // It means that the request is being processed.
+        this.isDocumentRequestCached = false;
+
+        const {
+            frameId,
+            requestId,
+            requestUrl,
+            requestType,
+        } = requestContext;
+
+        if (requestType === RequestType.Document) {
+            this.handleMainFrameRequest(requestUrl, requestId, isRemoveparamRedirect);
+        } else {
+            this.frames.set(frameId, new Frame(requestUrl, requestId));
+        }
+    }
+
+    /**
+     * Handles request {@link MatchingResult} from WebRequest onBeforeRequest handler
+     * and stores it in specified frame context.
+     *
+     * @param frameId Frame id.
+     * @param matchingResult Matching result.
+     */
+    public handleFrameMatchingResult(frameId: number, matchingResult: MatchingResult | null): void {
+        const frame = this.frames.get(frameId);
+
+        if (frame) {
+            frame.matchingResult = matchingResult;
+        }
+    }
+
+    /**
+     * Handles frame {@link CosmeticResult} from WebRequest onBeforeRequest handler
+     * and stores it in specified frame context.
+     *
+     * @param frameId Frame id.
+     * @param cosmeticResult Cosmetic result.
+     */
+    public handleFrameCosmeticResult(frameId: number, cosmeticResult: CosmeticResult): void {
+        const frame = this.frames.get(frameId);
+
+        if (frame) {
+            frame.cosmeticResult = cosmeticResult;
+        }
+    }
+
+    /**
+     * Handles document request and updates main frame context.
+     *
+     * Also matches document level rule and store it {@link mainFrameRule}.
+     *
+     * MatchingResult handles in {@link handleFrameMatchingResult}.
+     * CosmeticResult handles in {@link handleFrameCosmeticResult}.
+     *
+     * @param requestUrl Request url.
+     * @param requestId Request id.
+     * @param isRemoveparamRedirect Indicates whether the request is a $removeparam redirect.
+     */
+    private handleMainFrameRequest(
+        requestUrl: string,
+        requestId?: string,
+        isRemoveparamRedirect = false,
+    ): void {
+        // Clear frames data on tab reload.
+        this.frames.clear();
+
+        // Set new main frame data.
+        this.frames.set(MAIN_FRAME_ID, new Frame(requestUrl, requestId));
+
+        // Calculate new main frame rule.
+        this.mainFrameRule = engineApi.matchFrame(requestUrl);
+        // Reset tab blocked count.
+        this.blockedRequestCount = 0;
+
+        if (!isRemoveparamRedirect) {
+            // dispatch filtering log reload event
+            this.filteringLog.publishEvent({
+                type: FilteringEventType.TabReload,
+                data: {
+                    tabId: this.info.id,
+                },
+            });
+        }
+    }
+
+    /**
+     * Creates context for new tab.
+     *
+     * @param tab Webextension API tab data.
+     *
+     * @returns Tab context for new tab.
+     */
+    public static createNewTabContext(tab: TabInfo): TabContext {
+        const tabContext = new TabContext(tab);
+
+        // In some cases, tab is created while browser navigation processing.
+        // For example: when you navigate outside the browser or create new empty tab.
+        // `pendingUrl` represent url navigated to. We check it first.
+        // If server returns redirect, new main frame url will be processed in WebRequestApi.
+        const url = tab.pendingUrl || tab.url;
+
+        if (url && isHttpOrWsRequest(url)) {
+            tabContext.mainFrameRule = engineApi.matchFrame(url);
+
+            tabContext.frames.set(MAIN_FRAME_ID, new Frame(url));
+        }
+
+        return tabContext;
+    }
+
+    /**
+     * Checks if passed {@link Tabs.Tab} details represent a browser tab.
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/Tab#type
+     * @see https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/TAB_ID_NONE
+     *
+     * @param tab Tab details.
+     * @returns True if the tab is a browser tab, otherwise returns false.
+     */
+    public static isBrowserTab(tab: Tabs.Tab): tab is TabInfo {
+        return typeof tab.id === 'number' && tab.id !== browser.tabs.TAB_ID_NONE;
+    }
+}
