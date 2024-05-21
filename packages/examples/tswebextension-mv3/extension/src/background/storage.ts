b77fc8bf192dfa908cd5f333398d5995f46241a9
Maxim Topciu <mtopciu@adguard.com>
2024-05-21 18:48:22 +0300

    fix types for tswebextension mv3 example

--- packages/examples/tswebextension-mv3/extension/src/background/storage.ts
+++ packages/examples/tswebextension-mv3/extension/src/background/storage.ts
@@ -1,3 +1,5 @@
+import browser from 'webextension-polyfill';
+
 export const enum StorageKeys {
     IsStarted = 'isStarted',
     Config = 'config',
@@ -6,32 +8,19 @@ export const enum StorageKeys {
 class Storage {
     private storage;
 
-    constructor(storage: chrome.storage.LocalStorageArea) {
+    constructor(storage: browser.Storage.LocalStorageArea) {
         this.storage = storage;
     }
 
-    get = <T>(key: string): Promise<T | undefined> => {
-        return new Promise((resolve, reject) => {
-            this.storage.get([key], (result: { [x: string]: T }) => {
-                if (chrome.runtime.lastError) {
-                    reject(chrome.runtime.lastError);
-                }
-                resolve(result[key]);
-            });
-        });
+    get = async <T>(key: string): Promise<T | undefined> => {
+        const result = await this.storage.get([key]);
+        return result[key];
     };
 
     // TODO: Add throttle
-    set = (key: string, value: unknown): Promise<void> => {
-        return new Promise((resolve, reject) => {
-            this.storage.set({ [key]: value }, () => {
-                if (chrome.runtime.lastError) {
-                    reject(chrome.runtime.lastError);
-                }
-                resolve();
-            });
-        });
+    set = async (key: string, value: unknown): Promise<void> => {
+        await this.storage.set({ [key]: value });
     };
 }
 
-export const storage = new Storage(chrome.storage.local);
+export const storage = new Storage(browser.storage.local);
