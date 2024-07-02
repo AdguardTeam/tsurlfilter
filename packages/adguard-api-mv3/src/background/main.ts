d61f6ebf1a032da3d019fd6ac78c9be7f0119cba
Vladimir Zhelvis <v.zhelvis@adguard.com>
2024-07-02 15:23:12 +0300

    Pull request #917: AG-34016 Update Adguard API MV3
    Merge in ADGUARD-FILTERS/tsurlfilter from feature/AG-34016 to release/v2.3-for-5.0
    
    Squashed commit of the following:
    
    commit d08cb74a0e9ac62bbb5b7a4e54de0a227ef28256
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Tue Jul 2 14:54:53 2024 +0300
    
        fix comments
    
    commit 9a3ce0237d3d7a0f0ba287c9e520fd52b313af28
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Tue Jul 2 14:50:06 2024 +0300
    
        packages/adguard-api-mv3/CHANGELOG.md edited online with Bitbucket
    
    commit 2ec133d959e24d1b62c5691aabc2c6f87a350de6
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Tue Jul 2 14:49:58 2024 +0300
    
        packages/adguard-api-mv3/README.md edited online with Bitbucket
    
    commit 40688667e235c83b8433318f027cae4ed7734390
    Author: Atlassian Bamboo <bamboo@example.com>
    Date:   Tue Jul 2 14:13:17 2024 +0300
    
        skipci: Automatic update tsurlfilter and tswebextension tgz files
    
    commit 7eb949fd165b89c131b5518960e04645ab6da982
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Tue Jul 2 14:06:53 2024 +0300
    
        update lock file
    
    commit 8fe6c2d1e6a3f53e4cbedfe14180f943de6bba34
    Author: Atlassian Bamboo <bamboo@example.com>
    Date:   Tue Jul 2 13:37:22 2024 +0300
    
        skipci: Automatic update tsurlfilter and tswebextension tgz files
    
    commit a2eed99a329ac4a7e4b445fc20a7488cc1e06c75
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Tue Jul 2 13:30:01 2024 +0300
    
        freeze lerna version
    
    commit e4323f7df7470a28fa9b6c00dc03906d6162bdfb
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Tue Jul 2 13:22:31 2024 +0300
    
        fix indent size in package.json
    
    commit a06675671aa4d8a38c18bd6cecf4cfb04dc905b9
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Tue Jul 2 13:19:15 2024 +0300
    
        update lock file
    
    commit 0c703f7e4adc233c28c4cfff2a1cf5e6f6b226ce
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Tue Jul 2 13:18:09 2024 +0300
    
        add missed package json
    
    commit 71a1578432c441f849ab7f0ffae49638b5d4bace
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Tue Jul 2 13:15:01 2024 +0300
    
        use workspace protocol for linked deps
    
    commit da127aa9ea4814a0a48e0b3c55afbcdced9e27b7
    Author: Vladimir Zhelvis <v.zhelvis@adguard.com>
    Date:   Tue Jul 2 12:51:08 2024 +0300
    
        update Adguard APi mv3

--- packages/adguard-api-mv3/src/background/main.ts
+++ packages/adguard-api-mv3/src/background/main.ts
@@ -1,26 +1,28 @@
 /**
  * @file
- * This file is part of Adguard API library (https://github.com/AdguardTeam/tsurlfilter/packages/adguard-api).
+ * This file is part of Adguard API MV3 library (https://github.com/AdguardTeam/tsurlfilter/packages/adguard-api-mv3).
  *
- * Adguard API is free software: you can redistribute it and/or modify
+ * Adguard API MV3 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Adguard API is distributed in the hope that it will be useful,
+ * Adguard API MV3 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Adguard API. If not, see <http://www.gnu.org/licenses/>.
+ * along with Adguard API MV3. If not, see <http://www.gnu.org/licenses/>.
  */
+
 import {
     TsWebExtension,
     type Configuration as TsWebExtensionConfiguration,
     type MessagesHandlerMV3,
 } from '@adguard/tswebextension/mv3';
 import { type Configuration, configurationValidator } from './configuration';
+import { RequestBlockingLogger } from './request-blocking-logger';
 
 /**
  * AdGuard API is filtering library, provided following features:
@@ -41,11 +43,18 @@ export class AdguardApi {
     public onAssistantCreateRule: typeof this.tswebextension.onAssistantCreateRule;
 
     /**
+     * API for adding and removing listeners for request blocking events.
+     *
+     */
+    public onRequestBlocked: RequestBlockingLogger;
+
+    /**
      * Creates new AdguardApi instance.
      * @param tswebextension Instance of {@link TsWebExtension}.
      */
     constructor(private readonly tswebextension: TsWebExtension) {
         this.onAssistantCreateRule = this.tswebextension.onAssistantCreateRule;
+        this.onRequestBlocked = new RequestBlockingLogger();
 
         this.openAssistant = this.openAssistant.bind(this);
     }
---
+++ packages/adguard-api-mv3/src/background/request-blocking-logger.ts
@@ -0,0 +1,112 @@
+/**
+ * @file
+ * This file is part of Adguard API MV3 library (https://github.com/AdguardTeam/tsurlfilter/packages/adguard-api-mv3).
+ *
+ * Adguard API MV3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Adguard API MV3 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Adguard API MV3. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {
+    type ApplyBasicRuleEvent,
+    type ContentType,
+    defaultFilteringLog,
+    EventChannel,
+    type EventChannelListener,
+    FilteringEventType,
+} from '@adguard/tswebextension/mv3';
+
+export type RequestBlockingEvent = {
+    /** Tab identifier. */
+    tabId: number;
+    /**  Blocked request URL. */
+    requestUrl: string;
+    /**  Referrer URL. */
+    referrerUrl: string;
+    /** Assumed Filtering rule, which has blocked this request. */
+    assumedRule: string;
+    /** Assumed rule's filter identifier. */
+    assumedFilterId: number;
+    /** Request mime type. */
+    requestType: ContentType;
+};
+
+export interface RequestBlockingLoggerInterface {
+    addListener(listener: EventChannelListener<RequestBlockingEvent>): void;
+    removeListener(listener: EventChannelListener<RequestBlockingEvent>): void;
+}
+
+/**
+ * API for adding and removing listeners for request blocking events.
+ * Wraps {@link defaultFilteringLog} {@link EventChannel} for {@link ApplyBasicRuleEvent}.
+ */
+export class RequestBlockingLogger implements RequestBlockingLoggerInterface {
+    private channel = new EventChannel<RequestBlockingEvent>();
+
+    /**
+     * Create instance of {@link RequestBlockingLogger}.
+     */
+    constructor() {
+        this.onBasicRuleApply = this.onBasicRuleApply.bind(this);
+        defaultFilteringLog.addEventListener(FilteringEventType.ApplyBasicRule, this.onBasicRuleApply);
+    }
+
+    // map public API to event channel
+
+    /**
+     * Register listener for {@link RequestBlockingEvent}.
+     *
+     * @param listener Function to be called when {@link RequestBlockingEvent} is dispatched.
+     */
+    public addListener(listener: EventChannelListener<RequestBlockingEvent>): void {
+        this.channel.subscribe(listener);
+    }
+
+    /**
+     * Unregister listener for {@link RequestBlockingEvent}.
+     *
+     * @param listener Function to be called when {@link RequestBlockingEvent} is dispatched.
+     */
+    public removeListener(listener: EventChannelListener<RequestBlockingEvent>): void {
+        this.channel.unsubscribe(listener);
+    }
+
+    /**
+     * Handles {@link ApplyBasicRuleEvent}, gets extra data for {@link requestContextStorage}
+     * and dispatch new {@link RequestBlockingEvent}.
+     *
+     * @param event {@link ApplyBasicRuleEvent}.
+     */
+    private onBasicRuleApply(event: ApplyBasicRuleEvent): void {
+        const {
+            rule,
+            tabId,
+            requestUrl,
+            requestType,
+            frameUrl,
+        } = event.data;
+
+        // exclude allowlist rules
+        if (rule.isAllowlist()) {
+            return;
+        }
+
+        this.channel.dispatch({
+            tabId,
+            assumedRule: rule.getText(),
+            assumedFilterId: rule.getFilterListId(),
+            requestUrl,
+            referrerUrl: frameUrl,
+            requestType,
+        });
+    }
+}
