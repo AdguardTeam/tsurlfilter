eb238c799ebe84dac54483d7ca008decaef7d2af
Slava Leleka <v.leleka@adguard.com>
2024-09-03 15:48:24 +0300

    Revert "fix build delimiter for dnr-rulesets auto build"
    This reverts commit 3e565763b96121a4950f9a5b196e153ca0b727c0.

--- packages/dnr-rulesets/tasks/auto-deploy-increment.ts
+++ packages/dnr-rulesets/tasks/auto-deploy-increment.ts
@@ -5,9 +5,11 @@ import packageJson from '../package.json';
 const { version } = packageJson;
 
 /**
- * Delimiter between version from package.json and deployment date and time.
+ * Delimiter for build version that respects semver spec.
+ *
+ * @see https://semver.org/
  */
-const BUILD_DELIMITER = '--';
+const SEMVER_BUILD_DELIMITER = '+';
 
 /**
  * Utility for number formatting.
@@ -48,13 +50,13 @@ function getDeploymentDateString(): string {
 function updateDeployDate(version: string): string {
     const deploymentDateString = getDeploymentDateString();
 
-    const delimiterIndex = version.indexOf(BUILD_DELIMITER);
+    const delimiterIndex = version.indexOf(SEMVER_BUILD_DELIMITER);
 
     if (delimiterIndex > -1) {
         version = version.slice(0, delimiterIndex);
     }
 
-    return `${version}${BUILD_DELIMITER}${deploymentDateString}`;
+    return `${version}${SEMVER_BUILD_DELIMITER}${deploymentDateString}`;
 }
 
 packageJson.version = updateDeployDate(version);
