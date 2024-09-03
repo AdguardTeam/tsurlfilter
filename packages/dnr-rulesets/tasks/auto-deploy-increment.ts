e4852ecc12a2706dca4f68b9fd118d8bd914353f
Slava Leleka <v.leleka@adguard.com>
2024-09-03 16:35:23 +0300

    remake auto-deploy-increment script. AG-35244
    Squashed commit of the following:
    
    commit 769c3e537cb932b71b821cb3fbc8dc4b9a8afd51
    Author: Slava Leleka <v.leleka@adguard.com>
    Date:   Tue Sep 3 16:15:16 2024 +0300
    
        remake auto-deploy-increment script

--- packages/dnr-rulesets/tasks/auto-deploy-increment.ts
+++ packages/dnr-rulesets/tasks/auto-deploy-increment.ts
@@ -5,11 +5,9 @@ import packageJson from '../package.json';
 const { version } = packageJson;
 
 /**
- * Delimiter for build version that respects semver spec.
- *
- * @see https://semver.org/
+ * Version delimiter.
  */
-const SEMVER_BUILD_DELIMITER = '+';
+const VERSION_DELIMITER = '.';
 
 /**
  * Utility for number formatting.
@@ -22,42 +20,40 @@ function formatNumber(value: number): string {
 }
 
 /**
- * Returns deployment date string in format `yyyy-mm-dd-hh-MM-ss`.
+ * Returns current date and time (UTC) in format `yyyymmddhhMMss`.
  *
- * @returns Date string
+ * @returns Date and time string.
  */
 function getDeploymentDateString(): string {
     const now = new Date();
 
-    const day = formatNumber(now.getDate());
-    const month = formatNumber(now.getMonth() + 1); // Months are zero-based
-    const year = String(now.getFullYear());
-    const hours = formatNumber(now.getHours());
-    const minutes = formatNumber(now.getMinutes());
-    const seconds = formatNumber(now.getSeconds());
+    const day = formatNumber(now.getUTCDate());
+    const month = formatNumber(now.getUTCMonth() + 1); // Months are zero-based
+    const year = String(now.getUTCFullYear());
+    const hours = formatNumber(now.getUTCHours());
+    const minutes = formatNumber(now.getUTCMinutes());
+    const seconds = formatNumber(now.getUTCSeconds());
 
-    return `${year}-${month}-${day}-${hours}-${minutes}-${seconds}`;
+    return `${year}${month}${day}${hours}${minutes}${seconds}`;
 }
 
 /**
- * Updates package version with deployment date respecting the semver format.
+ * Updates package version where patch version is replaced with deployment date and time.
  *
  * @see https://semver.org/
  *
- * @param version current package version
- * @returns updated package version with deployment date
+ * @param version Current package version.
+ * @returns Updated package version.
  */
-function updateDeployDate(version: string): string {
+function updateVersion(version: string): string {
     const deploymentDateString = getDeploymentDateString();
 
-    const delimiterIndex = version.indexOf(SEMVER_BUILD_DELIMITER);
+    const minorPatchDelimiterIndex = version.lastIndexOf(VERSION_DELIMITER);
 
-    if (delimiterIndex > -1) {
-        version = version.slice(0, delimiterIndex);
-    }
+    const versionWithoutPatch = version.slice(0, minorPatchDelimiterIndex);
 
-    return `${version}${SEMVER_BUILD_DELIMITER}${deploymentDateString}`;
+    return `${versionWithoutPatch}${VERSION_DELIMITER}${deploymentDateString}`;
 }
 
-packageJson.version = updateDeployDate(version);
+packageJson.version = updateVersion(version);
 fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
