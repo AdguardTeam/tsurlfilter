e716b4acdb579d7b374e2b22fec0371afe63558a
scripthunter7 <d.tota@adguard.com>
2024-09-04 18:58:00 +0200

    basic network rule conversion to ubo syntax

--- packages/agtree/src/converter/network/index.ts
+++ packages/agtree/src/converter/network/index.ts
@@ -59,4 +59,46 @@ export class NetworkRuleConverter extends RuleConverterBase {
         // If the modifiers were not converted, return the original rule
         return createNodeConversionResult([rule], false);
     }
+
+    /**
+     * Converts a network rule to uBlock format, if possible.
+     *
+     * @param rule Rule node to convert
+     * @returns An object which follows the {@link NodeConversionResult} interface. Its `result` property contains
+     * the array of converted rule nodes, and its `isConverted` flag indicates whether the original rule was converted.
+     * If the rule was not converted, the result array will contain the original node with the same object reference
+     * @throws If the rule is invalid or cannot be converted
+     */
+    public static convertToUbo(rule: AnyNetworkRule): NodeConversionResult<NetworkRule> {
+        // TODO: add support for host rules
+        if (rule.type !== NetworkRuleType.NetworkRule) {
+            throw new Error(`Invalid rule type: ${rule.type}`);
+        }
+
+        if (rule.modifiers) {
+            const modifiers = NetworkRuleModifierListConverter.convertToUbo(rule.modifiers, rule.exception);
+
+            // If the object reference is different, it means that the modifiers were converted
+            // In this case, we should clone the entire rule and replace the modifiers with the converted ones
+            if (modifiers.isConverted) {
+                return {
+                    result: [{
+                        category: RuleCategory.Network,
+                        type: NetworkRuleType.NetworkRule,
+                        syntax: rule.syntax,
+                        exception: rule.exception,
+                        pattern: {
+                            type: 'Value',
+                            value: rule.pattern.value,
+                        },
+                        modifiers: modifiers.result,
+                    }],
+                    isConverted: true,
+                };
+            }
+        }
+
+        // If the modifiers were not converted, return the original rule
+        return createNodeConversionResult([rule], false);
+    }
 }
