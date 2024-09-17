082c9ef6f2c303cf570a83c9cef91d405329ff99
scripthunter7 <d.tota@adguard.com>
2024-09-17 17:11:54 +0200

    refactor plugin extractor

--- packages/agtree/browser-benchmark/helpers/build-agtree-node.ts
+++ packages/agtree/browser-benchmark/helpers/build-agtree-node.ts
@@ -3,24 +3,13 @@
  *
  * @note It uses the main Rollup configuration from the root of the project.
  */
-import { type Plugin, rollup } from 'rollup';
+import { rollup } from 'rollup';
 import path from 'path';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import swc from '@rollup/plugin-swc';
 
 import { commonPlugins } from '../../rollup.config';
-
-/**
- * Get a plugin from a list of plugins.
- *
- * @param plugins The list of plugins.
- * @param name The name of the plugin to get.
- *
- * @returns The plugin instance or `undefined` if not found.
- */
-const getPluginFromList = (plugins: Plugin[], name: string): Plugin | undefined => {
-    return plugins.find((plugin) => plugin.name === name);
-};
+import { extractRollupPlugins } from './extract-rollup-plugins';
 
 /**
  * Create an AGTree bundle for Node.js environment.
@@ -33,14 +22,7 @@ export const buildAgTreeForNode = async (): Promise<string> => {
     const bundle = await rollup({
         input: path.join(__dirname, '../../src/index.ts'),
         plugins: [
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            getPluginFromList(commonPlugins, 'json')!,
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            getPluginFromList(commonPlugins, 'alias')!,
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            getPluginFromList(commonPlugins, 'commonjs')!,
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            getPluginFromList(commonPlugins, 'node-externals')!,
+            ...extractRollupPlugins(commonPlugins, ['json', 'alias', 'commonjs', 'node-externals']),
 
             nodeResolve({
                 extensions: ['.js', '.ts'],
--- packages/agtree/browser-benchmark/helpers/build-iife.ts
+++ packages/agtree/browser-benchmark/helpers/build-iife.ts
@@ -3,23 +3,12 @@
  *
  * @note It uses the main Rollup configuration from the root of the project.
  */
-import { type Plugin, rollup } from 'rollup';
+import { rollup } from 'rollup';
 import swc from '@rollup/plugin-swc';
 import nodeResolve from '@rollup/plugin-node-resolve';
 
 import { commonPlugins } from '../../rollup.config';
-
-/**
- * Get a plugin from a list of plugins.
- *
- * @param plugins The list of plugins.
- * @param name The name of the plugin to get.
- *
- * @returns The plugin instance or `undefined` if not found.
- */
-const getPluginFromList = (plugins: Plugin[], name: string): Plugin | undefined => {
-    return plugins.find((plugin) => plugin.name === name);
-};
+import { extractRollupPlugins } from './extract-rollup-plugins';
 
 /**
  * Create an IIFE bundle for the browser environment.
@@ -35,12 +24,7 @@ export const buildIife = async (input: string, name: string): Promise<string> =>
     const bundle = await rollup({
         input,
         plugins: [
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            getPluginFromList(commonPlugins, 'json')!,
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            getPluginFromList(commonPlugins, 'alias')!,
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            getPluginFromList(commonPlugins, 'commonjs')!,
+            ...extractRollupPlugins(commonPlugins, ['json', 'alias', 'commonjs']),
 
             nodeResolve({
                 extensions: ['.js', '.ts'],
