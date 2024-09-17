8d9b4952ea8ecfe891cb66baef3e40d2d4913acb
scripthunter7 <d.tota@adguard.com>
2024-09-17 16:50:52 +0200

    optimize build for agtree

--- packages/agtree/browser-benchmark/helpers/build-agtree-node.ts
+++ packages/agtree/browser-benchmark/helpers/build-agtree-node.ts
@@ -3,14 +3,24 @@
  *
  * @note It uses the main Rollup configuration from the root of the project.
  */
-import { rollup } from 'rollup';
+import { type Plugin, rollup } from 'rollup';
 import path from 'path';
-import { fileURLToPath } from 'url';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import swc from '@rollup/plugin-swc';
 
-import { getNodePlugins } from '../../rollup.config';
+import { commonPlugins } from '../../rollup.config';
 
-// eslint-disable-next-line @typescript-eslint/naming-convention, no-underscore-dangle
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+/**
+ * Get a plugin from a list of plugins.
+ *
+ * @param plugins The list of plugins.
+ * @param name The name of the plugin to get.
+ *
+ * @returns The plugin instance or `undefined` if not found.
+ */
+const getPluginFromList = (plugins: Plugin[], name: string): Plugin | undefined => {
+    return plugins.find((plugin) => plugin.name === name);
+};
 
 /**
  * Create an AGTree bundle for Node.js environment.
@@ -21,8 +31,23 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
  */
 export const buildAgTreeForNode = async (): Promise<string> => {
     const bundle = await rollup({
-        input: path.join(__dirname, '..', '..', 'src', 'index.ts'),
-        plugins: getNodePlugins(true),
+        input: path.join(__dirname, '../../src/index.ts'),
+        plugins: [
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            getPluginFromList(commonPlugins, 'json')!,
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            getPluginFromList(commonPlugins, 'alias')!,
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            getPluginFromList(commonPlugins, 'commonjs')!,
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            getPluginFromList(commonPlugins, 'node-externals')!,
+
+            nodeResolve({
+                extensions: ['.js', '.ts'],
+            }),
+
+            swc(),
+        ],
     });
 
     const { output } = await bundle.generate({
--- packages/agtree/browser-benchmark/helpers/build-iife.ts
+++ packages/agtree/browser-benchmark/helpers/build-iife.ts
@@ -3,9 +3,23 @@
  *
  * @note It uses the main Rollup configuration from the root of the project.
  */
-import { rollup } from 'rollup';
+import { type Plugin, rollup } from 'rollup';
+import swc from '@rollup/plugin-swc';
+import nodeResolve from '@rollup/plugin-node-resolve';
 
 import { commonPlugins } from '../../rollup.config';
+
+/**
+ * Get a plugin from a list of plugins.
+ *
+ * @param plugins The list of plugins.
+ * @param name The name of the plugin to get.
+ *
+ * @returns The plugin instance or `undefined` if not found.
+ */
+const getPluginFromList = (plugins: Plugin[], name: string): Plugin | undefined => {
+    return plugins.find((plugin) => plugin.name === name);
+};
 
 /**
  * Create an IIFE bundle for the browser environment.
@@ -20,7 +34,21 @@ import { commonPlugins } from '../../rollup.config';
 export const buildIife = async (input: string, name: string): Promise<string> => {
     const bundle = await rollup({
         input,
-        plugins: commonPlugins,
+        plugins: [
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            getPluginFromList(commonPlugins, 'json')!,
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            getPluginFromList(commonPlugins, 'alias')!,
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            getPluginFromList(commonPlugins, 'commonjs')!,
+
+            nodeResolve({
+                extensions: ['.js', '.ts'],
+                browser: true,
+            }),
+
+            swc(),
+        ],
     });
 
     const { output } = await bundle.generate({
