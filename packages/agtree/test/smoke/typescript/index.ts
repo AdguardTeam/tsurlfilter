cf96c17efd7a6ecf992ff013fd19c7955376cb19
Dávid Tóta <d.tota@adguard.com>
2024-09-19 14:44:53 +0300

    AG-36016 Fix build size for CSS Tokenizer and AGTree
    Merge in ADGUARD-FILTERS/tsurlfilter from fix/AG-36016-1 to master
    
    Squashed commit of the following:
    
    commit ad53c103edf0129d57f83c68f31d1257c7247c44
    Merge: 6e2bb9ebe ff117a61c
    Author: scripthunter7 <d.tota@adguard.com>
    Date:   Thu Sep 19 13:32:55 2024 +0200
    
        Merge branch 'master' into fix/AG-36016-1
    
    commit 6e2bb9ebec5b10ccb4115e28e20276fe8ef50384
    Author: scripthunter7 <d.tota@adguard.com>
    Date:   Thu Sep 19 13:20:00 2024 +0200
    
        change operator
    
    commit 8cc6f2906bdb10a33e2e1dc86291e4ac6f09d621
    Author: scripthunter7 <d.tota@adguard.com>
    Date:   Thu Sep 19 10:11:53 2024 +0200
    
        bump version and changelog for css tokenizer
    
    commit da3ddaf77dd68f1290094e92a99fed7c14ba607a
    Author: scripthunter7 <d.tota@adguard.com>
    Date:   Thu Sep 19 10:11:43 2024 +0200
    
        bump version and changelog for agtree
    
    commit 026a507f6749dabbbfcc5c6df9b0b574a280e50d
    Author: scripthunter7 <d.tota@adguard.com>
    Date:   Thu Sep 19 10:10:00 2024 +0200
    
        smoke tests for css tokenizer
    
    commit 715e1b3f10ceac1772d46964284c31e7e592d227
    Author: scripthunter7 <d.tota@adguard.com>
    Date:   Thu Sep 19 10:00:10 2024 +0200
    
        small fix for css tokenizer build
    
    commit b3a2a146db51f7c875fb43cc076eeddc98e9d9b6
    Author: scripthunter7 <d.tota@adguard.com>
    Date:   Thu Sep 19 09:59:06 2024 +0200
    
        optimize compatibility tables build for agtree

--- packages/agtree/test/smoke/typescript/index.ts
+++ packages/agtree/test/smoke/typescript/index.ts
@@ -1,8 +1,12 @@
-import { RuleParser } from '@adguard/agtree';
+import { RuleParser, modifiersCompatibilityTable, SpecificPlatform } from '@adguard/agtree';
 import { ok } from 'assert';
 
 const ruleNode = RuleParser.parse('||example.com^');
 
 ok(ruleNode);
+
+const modifierData = modifiersCompatibilityTable.getSingle('third-party', SpecificPlatform.AdgExtChrome);
+
+ok(modifierData);
 
 console.log('Smoke test passed');
