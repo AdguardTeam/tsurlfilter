5d5d431476dcb8991cd1dda3027490ba8a5ef59b
Dmitriy Seregin <d.seregin@adguard.com>
2024-09-23 14:03:20 +0300

    AG-35746: run all tests in parallel

---
+++ bamboo-specs/scripts/tsurlfilter-update-docs-mv3.sh
@@ -0,0 +1,24 @@
+# This script only needed until release/v3.1 branch is not merged into master.
+# Purpose: faster link latest builds of tswebextension and tsurlfilter
+# right in repository without pushing to npm registry.
+set -e
+set -x
+
+# Fix mixed logs
+exec 2>&1
+
+echo "@adguard/tsurlfilter docs mv3 update starting"
+
+# import helper functions and some common variables
+. ./bamboo-specs/scripts/helpers.sh
+
+# Install dependencies
+pnpm install
+
+# Build with dependencies, lerna is used for builds caching, becuase
+# we specified dependencies with workspace links.
+npx lerna run build --scope @adguard/tsurlfilter --include-dependencies
+
+pnpm --filter @adguard/tsurlfilter docs:mv3
+
+echo "@adguard/tsurlfilter docs mv3 updated"
