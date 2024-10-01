cc1b277de5a001fcfabb9c04849822c890e217d3
scripthunter7 <d.tota@adguard.com>
2024-10-01 18:40:32 +0300

    update exports

--- packages/tsurlfilter/src/rules/declarative-converter/index.ts
+++ packages/tsurlfilter/src/rules/declarative-converter/index.ts
@@ -7,6 +7,7 @@ export * from './declarative-rule';
 export * from './constants/filenames';
 export * from './rules-hash-map';
 export * from './network-indexed-rule-with-hash';
+export * from './metadata-rule-extractor';
 export * from './errors/conversion-errors';
 export * from './errors/converter-options-errors';
 export * from './errors/limitation-errors';
