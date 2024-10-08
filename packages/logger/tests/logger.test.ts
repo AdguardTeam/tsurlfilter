463b270045a8a7b08ab400e432efdc36c7695b56
Ruslan Kurbanali <r.kurbanali@adguard.com>
2024-10-08 12:19:15 +0300

    AG-36533 Added new `LogLevel.Trace` logging level
    Merge in ADGUARD-FILTERS/tsurlfilter from feature/AG-36533 to master
    
    Squashed commit of the following:
    
    commit 44aaa2d84f8c0c1d399525bf8521cab96b9760d2
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Tue Oct 8 11:03:10 2024 +0500
    
        added jsdoc
    
    commit 2cc75251ec1189b0b6e8a12d432868434802c65d
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Tue Oct 8 10:58:33 2024 +0500
    
        updated readme
    
    commit 398c980e15b4bba9a14e40ef1f447420d01c0c28
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Tue Oct 8 10:57:14 2024 +0500
    
        updated changelog date
    
    commit 2c1d6e440e22e32db204d91f997f788488caaee4
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Mon Oct 7 13:43:22 2024 +0500
    
        bump version, updated changelog and readme
    
    commit 152091969e05c7b1c2e85813d8baba1c6fa5abb0
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Mon Oct 7 13:36:16 2024 +0500
    
        proper checking writer.trace method
    
    commit 021bd7a5d395dfde8365ad4ce038d7b9442d8331
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Mon Oct 7 11:41:08 2024 +0500
    
        updated comments
    
    commit 8cb7a3ec21d23bd31bcb1660d8607851d72c871d
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Mon Oct 7 11:40:02 2024 +0500
    
        updated tests
    
    commit a31fbe77d9953a5c422e3d958a1014966d762328
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Mon Oct 7 11:39:49 2024 +0500
    
        updated changelog
    
    commit de5a6dc54ceecd34ecef261fb859b456dcba2753
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Mon Oct 7 11:36:12 2024 +0500
    
        updated readme
    
    commit 1ad9d8ed1f700ff2023896b0e6b8acc4b44c53de
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Mon Oct 7 11:36:05 2024 +0500
    
        updated changelog
    
    commit 937facaa97563a095074bb3da18fe7a323e787d6
    Author: Kurbanali Ruslan <r.kurbanali@adguard.com>
    Date:   Mon Oct 7 11:33:03 2024 +0500
    
        added trace logging level

--- packages/logger/tests/logger.test.ts
+++ packages/logger/tests/logger.test.ts
@@ -62,8 +62,11 @@ describe('works', () => {
             const logger = new Logger(writer);
             expect(logger.currentLevel).toBe(LogLevel.Info);
         });
+
         it('switches log levels', () => {
             const logger = new Logger(writer);
+            logger.currentLevel = LogLevel.Trace;
+            expect(logger.currentLevel).toBe(LogLevel.Trace);
             logger.currentLevel = LogLevel.Debug;
             expect(logger.currentLevel).toBe(LogLevel.Debug);
             logger.currentLevel = LogLevel.Info;
@@ -71,6 +74,7 @@ describe('works', () => {
             logger.currentLevel = LogLevel.Error;
             expect(logger.currentLevel).toBe(LogLevel.Error);
         });
+
         it('does not print message if debug is printed and info is selected', () => {
             const logger = new Logger(writer);
             const message = 'some message';
@@ -80,6 +84,7 @@ describe('works', () => {
             expect(writer.log).not.toHaveBeenCalled();
             expect(writer.error).not.toHaveBeenCalled();
         });
+
         it('prints message if debug method is called and debug level is selected', () => {
             const logger = new Logger(writer);
             const message = 'some message';
@@ -88,6 +93,66 @@ describe('works', () => {
             expect(writer.info).not.toHaveBeenCalled();
             expect(writer.log).toHaveBeenCalled();
             expect(writer.error).not.toHaveBeenCalled();
+        });
+
+        describe('log level -- trace', () => {
+            const writerWithTrace: Writer = {
+                log: jest.fn(),
+                info: jest.fn(),
+                error: jest.fn(),
+                trace: jest.fn(),
+            };
+
+            it('does not print with trace method if error is called', () => {
+                const logger = new Logger(writerWithTrace);
+                const message = 'some message';
+                logger.currentLevel = LogLevel.Trace;
+                logger.error(message);
+                expect(writerWithTrace.error).toHaveBeenCalled();
+                expect(writerWithTrace.trace).not.toHaveBeenCalled();
+            });
+
+            it('does not print with trace method if level is not enough', () => {
+                const logger = new Logger(writerWithTrace);
+                const message = 'some message';
+                logger.currentLevel = LogLevel.Debug;
+
+                logger.debug(message);
+                expect(writerWithTrace.log).toHaveBeenCalled();
+
+                logger.info(message);
+                logger.warn(message);
+                expect(writerWithTrace.info).toHaveBeenCalledTimes(2);
+
+                expect(writerWithTrace.trace).not.toHaveBeenCalled();
+            });
+
+            it('print with regular methods if trace method is not provided and level is enough', () => {
+                const logger = new Logger(writer);
+                const message = 'some message';
+                logger.currentLevel = LogLevel.Trace;
+
+                logger.debug(message);
+                expect(writer.log).toHaveBeenCalled();
+
+                logger.info(message);
+                logger.warn(message);
+                expect(writer.info).toHaveBeenCalledTimes(2);
+            });
+
+            it('print with trace method', () => {
+                const logger = new Logger(writerWithTrace);
+                const message = 'some message';
+                logger.currentLevel = LogLevel.Trace;
+
+                logger.debug(message);
+                logger.info(message);
+                logger.warn(message);
+                logger.error(message);
+
+                expect(writerWithTrace.trace).toHaveBeenCalledTimes(3);
+                expect(writerWithTrace.error).toHaveBeenCalled();
+            });
         });
     });
 });
