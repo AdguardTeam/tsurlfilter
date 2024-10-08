42a1c71d71d671f276fd95328619b8e00675121f
scripthunter7 <d.tota@adguard.com>
2024-10-08 13:19:12 +0300

    refactor resource fetchers

--- packages/tsurlfilter/src/utils/resource-fetch.ts
+++ packages/tsurlfilter/src/utils/resource-fetch.ts
@@ -3,10 +3,10 @@ import { type ByteRange } from '../rules/declarative-converter/byte-range-map';
 /**
  * Fetches a resource from the given URL, optionally with a specified byte range.
  *
- * @param {string} url The URL of the resource to fetch.
- * @param {ByteRange} [range] Optional byte range to specify in the HTTP request.
+ * @param url The URL of the resource to fetch.
+ * @param [range] Optional byte range to specify in the HTTP request.
  *
- * @returns {Promise<Response>} A promise that resolves to the fetch response.
+ * @returns A promise that resolves to the fetch response.
  */
 export const fetchExtensionResource = async (url: string, range?: ByteRange): Promise<Response> => {
     if (range) {
@@ -22,10 +22,10 @@ export const fetchExtensionResource = async (url: string, range?: ByteRange): Pr
 /**
  * Fetches a resource from the given URL and returns its content as text, optionally with a specified byte range.
  *
- * @param {string} url The URL of the resource to fetch.
- * @param {ByteRange} [range] Optional byte range to specify in the HTTP request.
+ * @param url The URL of the resource to fetch.
+ * @param [range] Optional byte range to specify in the HTTP request.
  *
- * @returns {Promise<string>} A promise that resolves to the fetched content as a string.
+ * @returns A promise that resolves to the fetched content as a string.
  */
 export const fetchExtensionResourceText = async (url: string, range?: ByteRange): Promise<string> => {
     return (await fetchExtensionResource(url, range)).text();
