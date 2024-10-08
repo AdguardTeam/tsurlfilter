c72c04bc38a96adc6f7b31710a71b1f2b5718b63
Dmitriy Seregin <d.seregin@adguard.com>
2024-10-08 14:11:14 +0300

    sync scripts with master

--- bamboo-specs/scripts/tsurlfilter-tswebextension-builds.sh
+++ bamboo-specs/scripts/tsurlfilter-tswebextension-builds.sh
@@ -1,10 +1,13 @@
-# This script only needed until release/v3.1 branch is not merged into master.
-# Purpose: faster link latest builds of tswebextension and tsurlfilter
-# right in repository without pushing to npm registry.
-set -e
-set -x
+#!/bin/bash
 
-# Fix mixed logs
+# 'set' should be added to the beginning of each script to ensure that it runs with the correct options.
+# Please do not move it to some common file, like `setup-tests.sh`, because sourcing A script from B script
+# cannot change the options of B script.
+#  -e: Exit immediately if any command exits with a non-zero status (i.e., if a command fails).
+#  -x: Print each command to the terminal as it is executed, which is useful for debugging.
+set -ex
+
+# Redirect stderr (2) to stdout (1) to capture all output in a single log
 exec 2>&1
 
 echo "@adguard/tswebextension and @adguard/tsurlfilter builds starting"
