73eeb2dcd1d0cf361eab221da80fa357b9158a1b
Dmitriy Seregin <d.seregin@adguard.com>
2024-10-08 14:11:14 +0300

    sync scripts with master

--- bamboo-specs/scripts/examples-adguard-api-mv3-tests.sh
+++ bamboo-specs/scripts/examples-adguard-api-mv3-tests.sh
@@ -1,7 +1,13 @@
-set -e
-set -x
+#!/bin/bash
 
-# Fix mixed logs
+# 'set' should be added to the beginning of each script to ensure that it runs with the correct options.
+# Please do not move it to some common file, like `setup-tests.sh`, because sourcing A script from B script
+# cannot change the options of B script.
+#  -e: Exit immediately if any command exits with a non-zero status (i.e., if a command fails).
+#  -x: Print each command to the terminal as it is executed, which is useful for debugging.
+set -ex
+
+# Redirect stderr (2) to stdout (1) to capture all output in a single log
 exec 2>&1
 
 echo "adguard-api-mv3-example tests starting"
