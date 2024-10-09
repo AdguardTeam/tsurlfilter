3377cae2e2da7abf9b4d06d5e4766e618a1335da
scripthunter7 <d.tota@adguard.com>
2024-10-09 17:33:57 +0300

    refactor resource fetcher

--- packages/tsurlfilter/src/utils/resource-fetch.ts
+++ packages/tsurlfilter/src/utils/resource-fetch.ts
@@ -1,25 +1,6 @@
 import { type ByteRange } from '../rules/declarative-converter/byte-range-map';
 
 /**
- * Fetches a resource from the given URL, optionally with a specified byte range.
- *
- * @param url The URL of the resource to fetch.
- * @param [range] Optional byte range to specify in the HTTP request.
- *
- * @returns A promise that resolves to the fetch response.
- */
-export const fetchExtensionResource = async (url: string, range?: ByteRange): Promise<Response> => {
-    if (range) {
-        return fetch(url, {
-            headers: {
-                Range: `bytes=${range.start}-${range.end}`,
-            },
-        });
-    }
-    return fetch(url);
-};
-
-/**
  * Fetches a resource from the given URL and returns its content as text, optionally with a specified byte range.
  *
  * @param url The URL of the resource to fetch.
@@ -28,5 +9,7 @@ export const fetchExtensionResource = async (url: string, range?: ByteRange): Pr
  * @returns A promise that resolves to the fetched content as a string.
  */
 export const fetchExtensionResourceText = async (url: string, range?: ByteRange): Promise<string> => {
-    return (await fetchExtensionResource(url, range)).text();
+    const headers = range ? { Range: `bytes=${range.start}-${range.end}` } : undefined;
+    const response = await fetch(url, { headers });
+    return response.text();
 };
