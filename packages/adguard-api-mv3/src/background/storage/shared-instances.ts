0225ae5c72c19c550f002a487c6e13744b839768
scripthunter7 <d.tota@adguard.com>
2024-10-16 19:08:29 +0300

    Add JSDoc for shared storage instances

--- packages/adguard-api-mv3/src/background/storage/shared-instances.ts
+++ packages/adguard-api-mv3/src/background/storage/shared-instances.ts
@@ -1,16 +1,32 @@
 import { IDBStorage } from './idb';
 
+/**
+ * The name of the IndexedDB database for AdGuard API.
+ */
 const ADGUARD_API_IDB_NAME = 'adguardApiIDB';
 
+/**
+ * The name of the filters database within the AdGuard API IndexedDB.
+ */
 const ADGUARD_API_FILTERS_DB_NAME = 'filters';
+
+/**
+ * The name of the versions database within the AdGuard API IndexedDB.
+ */
 const ADGUARD_API_VERSIONS_DB_NAME = 'versions';
 
+/**
+ * An instance of IDBStorage for storing filter data.
+ */
 export const filtersIdbStorage = new IDBStorage(
     ADGUARD_API_FILTERS_DB_NAME,
     IDBStorage.DEFAULT_IDB_VERSION,
     ADGUARD_API_IDB_NAME,
 );
 
+/**
+ * An instance of IDBStorage for storing version data.
+ */
 export const versionsIdbStorage = new IDBStorage(
     ADGUARD_API_VERSIONS_DB_NAME,
     IDBStorage.DEFAULT_IDB_VERSION,
