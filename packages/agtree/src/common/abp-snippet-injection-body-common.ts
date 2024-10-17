7c47434365870459cfae3f3ba39720e5101538fe
Maxim Topciu <mtopciu@adguard.com>
2024-10-17 13:21:47 +0300

    AG-36861 get rid of side-effect in the agent serializer

--- packages/agtree/src/common/abp-snippet-injection-body-common.ts
+++ packages/agtree/src/common/abp-snippet-injection-body-common.ts
@@ -1,4 +1,3 @@
-// FIXME the same error message is in the parser too, so it needs to be moved to some common directory
 export class AbpSnippetInjectionBodyCommon {
     /**
      * Error messages used by the parser and generator.
