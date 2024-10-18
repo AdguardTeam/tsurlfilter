4d2cc26e107d94d5b61718b44c6f083e698e6040
Kurbanali Ruslan <r.kurbanali@adguard.com>
2024-10-18 16:18:09 +0500

    added header utils

--- packages/tswebextension/src/lib/common/utils/headers.ts
+++ packages/tswebextension/src/lib/common/utils/headers.ts
@@ -22,6 +22,30 @@ export function findHeaderByName(headers: HttpHeaders, headerName: string): Http
 }
 
 /**
+ * Identifies is header with name present in headers collection (case-insensitive).
+ *
+ * @param headers Headers collection.
+ * @param headerName Header name.
+ * @returns True if header by name is presented.
+ */
+export function hasHeaderByName(headers: HttpHeaders, headerName: string): boolean {
+    const header = findHeaderByName(headers, headerName);
+    return header !== null;
+}
+
+/**
+ * Identifies is matching header present in headers collection (header name is case-insensitive).
+ *
+ * @param headers Headers collection.
+ * @param header Header object.
+ * @returns True if header is present.
+ */
+export function hasHeader(headers: HttpHeaders, header: HttpHeadersItemType): boolean {
+    const foundHeader = findHeaderByName(headers, header.name);
+    return !!foundHeader && foundHeader.value === header.value;
+}
+
+/**
  * TODO: Make function pure (don't modify headers, return new object instead)
  * Removes header from headers by name.
  *
--- packages/tswebextension/test/lib/common/utils/headers.test.ts
+++ packages/tswebextension/test/lib/common/utils/headers.test.ts
@@ -1,4 +1,9 @@
-import { findHeaderByName, removeHeader } from '../../../../src/lib/common/utils/headers';
+import {
+    findHeaderByName,
+    hasHeaderByName,
+    hasHeader,
+    removeHeader,
+} from '../../../../src/lib/common/utils/headers';
 
 describe('Headers utils', () => {
     it('finds header by name', () => {
@@ -25,6 +30,65 @@ describe('Headers utils', () => {
         ], 'test_name');
         expect(result).not.toBeNull();
         expect(result!.name).toBe('test_name');
+    });
+
+    it('identifies is header included by name', () => {
+        let result = hasHeaderByName([], 'test_name');
+        expect(result).toBe(false);
+
+        result = hasHeaderByName([
+            {
+                name: 'an_other_name',
+                value: 'an_other_value',
+            },
+        ], 'test_name');
+        expect(result).toBe(false);
+
+        result = hasHeaderByName([
+            {
+                name: 'test_name',
+                value: 'test_value',
+            },
+            {
+                name: 'an_other_name',
+                value: 'an_other_value',
+            },
+        ], 'test_name');
+        expect(result).toBe(true);
+    });
+
+    it('identifies is header included', () => {
+        let result = hasHeader([], {
+            name: 'test_name',
+            value: 'test_value',
+        });
+        expect(result).toBe(false);
+
+        result = hasHeader([
+            {
+                name: 'an_other_name',
+                value: 'an_other_value',
+            },
+        ], {
+            name: 'test_name',
+            value: 'test_value',
+        });
+        expect(result).toBe(false);
+
+        result = hasHeader([
+            {
+                name: 'test_name',
+                value: 'test_value',
+            },
+            {
+                name: 'an_other_name',
+                value: 'an_other_value',
+            },
+        ], {
+            name: 'test_name',
+            value: 'test_value',
+        });
+        expect(result).toBe(true);
     });
 
     it('removes header by name', () => {
