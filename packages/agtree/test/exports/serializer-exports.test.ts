ff865b3728e40e27661cd9d8a492e64fa6b13ab2
Maxim Topciu <mtopciu@adguard.com>
2024-10-22 11:24:27 +0300

    AG-36861 reexport generator

--- packages/agtree/test/exports/serializer-exports.test.ts
+++ packages/agtree/test/exports/serializer-exports.test.ts
@@ -1,19 +1,35 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-ignore
-import { RuleSerializer } from '@adguard/agtree/serializer';
+import { RuleParser } from '@adguard/agtree/parser';
 // @ts-ignore
 import { RuleGenerator } from '@adguard/agtree/generator';
+// @ts-ignore
+import { RuleSerializer } from '@adguard/agtree/serializer';
+// @ts-ignore
+import { RuleDeserializer } from '@adguard/agtree/deserializer';
 
 describe('specific exports', () => {
-    describe('RuleSerializer', () => {
-        it('should export RuleSerializer', async () => {
-            expect(RuleSerializer).toBeDefined();
+    describe('RuleParser', () => {
+        it('should export RuleParser', async () => {
+            expect(RuleParser).toBeDefined();
         });
     });
 
     describe('RuleGenerator', () => {
         it('should export RuleGenerator', async () => {
             expect(RuleGenerator).toBeDefined();
+        });
+    });
+
+    describe('RuleSerializer', () => {
+        it('should export RuleSerializer', async () => {
+            expect(RuleSerializer).toBeDefined();
+        });
+    });
+
+    describe('RuleDeserializer', () => {
+        it('should export RuleDeserializer', () => {
+            expect(RuleDeserializer).toBeDefined();
         });
     });
 });
