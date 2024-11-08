967d9c39c5f4c215f2ccc069f638a4c2a3141064
Maxim Topciu <mtopciu@adguard.com>
2024-11-08 13:06:01 +0200

    AG-36861 fix imports

--- packages/agtree/src/converter/index.ts
+++ packages/agtree/src/converter/index.ts
@@ -1,8 +1,5 @@
 /**
  * @file Converter entry point
  */
-
-// FIXME add an entry point to the convertor
-// used in the ios-webextension
 export { RawFilterListConverter } from './raw-filter-list';
 export { RuleConverter } from './rule';
