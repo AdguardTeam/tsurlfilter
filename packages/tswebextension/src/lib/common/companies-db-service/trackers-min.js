508629758e318b1588138c9025279727838a11e0
Atlassian Bamboo <bamboo@example.com>
2024-11-19 11:00:45 +0300

    skipci: update companiesdb data

--- packages/tswebextension/src/lib/common/companies-db-service/trackers-min.js
+++ packages/tswebextension/src/lib/common/companies-db-service/trackers-min.js
@@ -1,5 +1,5 @@
 export const rawCompaniesDb = {
-	"timeUpdated": "2024-11-11T10:04:44.705Z",
+	"timeUpdated": "2024-11-18T10:05:14.225Z",
 	"categories": {
 		"0": "audio_video_player",
 		"1": "comments",
