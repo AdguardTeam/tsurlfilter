5d79e3bd6ab8d9696a00022ca027fc3d4dd88a19
Maxim Topciu <mtopciu@adguard.com>
2024-11-26 15:49:26 +0200

    fix import error with lru_map without wrapper

--- packages/tsurlfilter/src/engine/lru-wrapper.ts
+++
@@ -1,9 +0,0 @@
-import { createRequire } from 'module';
-import type { LRUMap as LRUMapType } from 'lru_map';
-
-const require = createRequire(import.meta.url);
-const { LRUMap } = require('lru_map');
-
-// Export the value and type simultaneously
-export { LRUMap };
-export type LRUMap<K, V> = LRUMapType<K, V>;
