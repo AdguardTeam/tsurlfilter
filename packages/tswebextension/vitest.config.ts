498b2f39593975cf52f0c043fda13d1433379b1b
Slava Leleka <v.leleka@adguard.com>
2024-12-12 16:41:34 +0200

    fix vitest config, fix more test imports

--- packages/tswebextension/vitest.config.ts
+++ packages/tswebextension/vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
-        globals: true, // Enables global access to describe, it, expect, etc.
         environment: 'jsdom', // Enables jsdom environment for tests.
         setupFiles: './vitest.setup.ts', // Setup file for tests.
     },
