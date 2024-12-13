a38ad21289b7bd81a4897750520630d2c19ad47d
jellizaveta <e.egorova@adguard.com>
2024-12-13 19:06:01 +0300

    add jsDocs

--- packages/agtree/src/parser/css/has-token.ts
+++ packages/agtree/src/parser/css/has-token.ts
@@ -1,5 +1,8 @@
 import { type TokenType, tokenizeExtended } from '@adguard/css-tokenizer';
 
+/**
+ * Represents an error that occurs when an operation is aborted.
+ */
 class AbortError extends Error {
     constructor() {
         super('Aborted');
@@ -7,6 +10,14 @@ class AbortError extends Error {
 }
 
 // #TODO: add a stop function to the tokenizers callback
+/**
+ * Checks if the given raw string contains any of the specified tokens.
+ *
+ * @param raw - The raw string to be tokenized and checked.
+ * @param tokens - A set of token types to check for in the raw string.
+ * @returns `true` if any of the specified tokens are found in the raw string, otherwise `false`.
+ * @throws {AbortError} If a specified token is found during tokenization.
+ */
 export const hasToken = (raw: string, tokens: Set<TokenType>): boolean => {
     try {
         tokenizeExtended(
