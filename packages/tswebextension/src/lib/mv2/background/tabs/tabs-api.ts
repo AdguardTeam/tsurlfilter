de1cdffbdf9bbe964b9d9e22cb43973200fc7e25
Slava Leleka <v.leleka@adguard.com>
2024-12-13 18:08:31 +0200

    revert fixme with better wording

--- packages/tswebextension/src/lib/mv2/background/tabs/tabs-api.ts
+++ packages/tswebextension/src/lib/mv2/background/tabs/tabs-api.ts
@@ -155,6 +155,8 @@ export class TabsApi {
         return this.context.get(tabId);
     }
 
+    // FIXME (Slava, in another pr): do the same in MV3
+    // since cosmetic rules should not be applied in the assistant frame
     /**
      * Sets a current timestamp as `assistantInitTimestamp` of the tab context.
      *
