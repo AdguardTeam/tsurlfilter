f9cdc7bdad790ce910ec70eed825158bfeb70c42
jellizaveta <e.egorova@adguard.com>
2024-12-16 11:39:23 +0300

    fix jsDoc

--- packages/agtree/src/parser/css/has-token.ts
+++ packages/agtree/src/parser/css/has-token.ts
@@ -12,11 +12,11 @@ class AbortError extends Error {
 // TODO: add a stop function to the tokenizers callback
 /**
  * Checks if the given raw string contains any of the specified tokens.
+ * This function uses error throwing inside the abort tokenization process.
  *
  * @param raw - The raw string to be tokenized and checked.
  * @param tokens - A set of token types to check for in the raw string.
  * @returns `true` if any of the specified tokens are found in the raw string, otherwise `false`.
- * @throws {AbortError} If a specified token is found during tokenization.
  */
 export const hasToken = (raw: string, tokens: Set<TokenType>): boolean => {
     try {
