27ed6d5f2fb12fa97bc24dc37e05b595427a9c61
Maxim Topciu <mtopciu@adguard.com>
2024-12-17 19:36:14 +0300

    Pull request 1137: Prepare @adguard/agtree@3.0.0-alpha.2 release
    Squashed commit of the following:
    
    commit 1364e02845eb46a0a948d39d8bb2bfc386b90db8
    Author: Maxim Topciu <mtopciu@adguard.com>
    Date:   Tue Dec 17 18:14:38 2024 +0200
    
        Update CHANGELOG to include links for alpha releases and clarify ESM package transition
    
    commit c20eaa65620f27281cbc02159db5acf585ce2eb5
    Author: Maxim Topciu <mtopciu@adguard.com>
    Date:   Tue Dec 17 18:11:05 2024 +0200
    
        update changelog
    
    commit 0fa6a668562cdca4c754f951ac24040a1e569b22
    Author: Maxim Topciu <mtopciu@adguard.com>
    Date:   Tue Dec 17 18:02:52 2024 +0200
    
        add FilterListParser export to @adguard/agtree/parser entrypoint
    
    commit c288580ef0bd6a2a4e54a5fb2f75e41bbbddc13f
    Author: Maxim Topciu <mtopciu@adguard.com>
    Date:   Tue Dec 17 17:57:39 2024 +0200
    
        Update test-adguard-agtree-3.0.0-alpha.2.tgz binary file
    
    commit f2890925130cc88f7c011c2f6567bdd0fa17e8d9
    Merge: 54374a5e5 608d64370
    Author: Maxim Topciu <mtopciu@adguard.com>
    Date:   Tue Dec 17 17:46:34 2024 +0200
    
        Merge branch 'release/v3.1' into fix/prepare-agtree-release-alpha
    
    commit 54374a5e553f302f610c429aeed1a89cddbb727e
    Author: Maxim Topciu <mtopciu@adguard.com>
    Date:   Fri Dec 13 17:22:07 2024 +0200
    
        update agtree tgz
    
    commit a516ffc24d0ffee2e3785c8755f57eb26a789245
    Merge: 151f2e460 57f1fcc0d
    Author: Maxim Topciu <mtopciu@adguard.com>
    Date:   Fri Dec 13 17:19:55 2024 +0200
    
        Merge branch 'release/v3.1' into fix/prepare-agtree-release-alpha
    
    commit 151f2e46058712d459ce722d2a9520d7b557f2a9
    Author: Maxim Topciu <mtopciu@adguard.com>
    Date:   Thu Dec 5 16:19:54 2024 +0200
    
        build tgz
    
    commit 4d51872bd0e4e1609f4d55ca81178f7251694172
    Author: Maxim Topciu <mtopciu@adguard.com>
    Date:   Thu Dec 5 16:16:03 2024 +0200
    
        update changelog

--- packages/agtree/src/parser/index.ts
+++ packages/agtree/src/parser/index.ts
@@ -3,3 +3,4 @@ export { defaultParserOptions, type ParserOptions } from './options';
 export { CosmeticRuleParser } from './cosmetic/cosmetic-rule-parser';
 export { DomainListParser } from './misc/domain-list-parser';
 export { NetworkRuleParser } from './network/network-rule-parser';
+export { FilterListParser } from './filterlist-parser';
