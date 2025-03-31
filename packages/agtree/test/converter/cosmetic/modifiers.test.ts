2a24d2356535e803329314189c7458e4c4bbdbf7
Elizaveta Egorova <e.egorova@adguard.com>
2025-03-31 18:42:23 +0300

    AG-40385 Handle ADG cosmetic rule modifiers in AGTree converter when converting to UBO syntax
    Squashed commit of the following:
    
    commit 633d62f4c3e7ff06d89abb48ff6fe4d0b73ae248
    Merge: c49689e4c f73f3b2da
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Mon Mar 31 18:19:51 2025 +0300
    
        Merge branch 'master' into fix/AG-40385
    
    commit c49689e4cd750926f2e7ee8680a7ed117340af1a
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Mon Mar 31 17:33:46 2025 +0300
    
        update tsurlfilter to 3.2.2
    
    commit 6b395cd278a9672ec9a19f6fc3b3892092fdaa90
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Mon Mar 31 16:06:35 2025 +0300
    
        add date in changelog
    
    commit ab7a6db0313c40e71b11e1426c7e903913ab0ced
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Mon Mar 31 13:12:20 2025 +0300
    
        refactoring
    
    commit c9b88898959c182ef0c63662c07584100ff2bb58
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Fri Mar 28 00:23:42 2025 +0300
    
        fix ensureSlashes, add constants
    
    commit 63b56d8901772cf7855c1fab49688b183d7ce778
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Thu Mar 27 16:43:41 2025 +0300
    
        remove comment
    
    commit 63e67dc99e9d9a97f6ab28f86aaec910d74c24d8
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Thu Mar 27 16:42:54 2025 +0300
    
        rewrite createDomainList
    
    commit f5bf77da25b05781ede9086b3ce887457858f35e
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Thu Mar 27 15:38:36 2025 +0300
    
        fix confusing comment
    
    commit d3e3aa65e478c650ae4b06db80a795b0c87ac187
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Wed Mar 26 17:27:32 2025 +0300
    
        add tests, fix functions in regexp utils, fix comment
    
    commit 9c16dca69849b9888206a669ddc978c30d8e47db
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Wed Mar 26 11:40:34 2025 +0300
    
        code review
    
    commit 16d9f15e6540fc123b66739f49bdbccda28d658f
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Mon Mar 24 16:40:30 2025 +0300
    
        update changelog
    
    commit e8ac6def2b137317eeea2f5d84388585c2d4ea1e
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Mon Mar 24 16:40:01 2025 +0300
    
        update changelog
    
    commit 787c933a62a35c7455ace61dce625e5e5447e091
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Mon Mar 24 16:39:15 2025 +0300
    
        code review
    
    commit 11839445361075c3ff3ac8ee88e60331f73be42e
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Mon Mar 24 11:48:02 2025 +0300
    
        refactor, changelog
    
    commit d3d06344649a5c67a9adb4d5be5eb7e00e2f9fbf
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Fri Mar 21 19:32:01 2025 +0300
    
        revert negation
    
    commit 0e22044c2669c25519917f12bb86fa6004f46d61
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Fri Mar 21 18:27:56 2025 +0300
    
        remove negation in matches-path modifier
    
    commit 33242b2658d371e0d16b3c41295ea7faf958b936
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Fri Mar 21 17:10:06 2025 +0300
    
        add cases for app and url modifiers
    
    commit b29a3dd157b2696f4f6546b0e30d225b3e1e355b
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Wed Mar 19 14:24:06 2025 +0300
    
        Handle case with regexp
    
    commit 0f3d2f4138ee37bd51c2f6496791845c3cf255f9
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Wed Mar 19 14:15:45 2025 +0300
    
        Handle domains modifier as regular domain list in Ubo
    
    commit 634dbaee66175d4d9b2c393fd5c0205473b51075
    Author: jellizaveta <e.egorova@adguard.com>
    Date:   Thu Mar 13 15:40:32 2025 +0300
    
        add test with empty value
    
    ... and 2 more commits

--- packages/agtree/test/converter/cosmetic/modifiers.test.ts
+++ packages/agtree/test/converter/cosmetic/modifiers.test.ts
@@ -144,4 +144,208 @@ describe('Cosmetic rule modifiers conversion', () => {
             expect(testData).toBeConvertedProperly(CosmeticRuleConverter, 'convertToAdg');
         });
     });
+    describe('ADG to uBO', () => {
+        test.each([
+            // [$path=....] modifier
+            // No value
+            {
+                actual: '[$path]example.com##.foo',
+                expected: [
+                    'example.com##:matches-path(/^/$/) .foo',
+                ],
+                shouldConvert: true,
+            },
+            // Basic cases
+            {
+                actual: '[$path=/bar]example.com##.foo',
+                expected: [
+                    'example.com##:matches-path(/bar) .foo',
+                ],
+                shouldConvert: true,
+            },
+            // Exception rule
+            {
+                actual: '[$path=/bar]example.com#@#.foo',
+                expected: [
+                    'example.com#@#:matches-path(/bar) .foo',
+                ],
+                shouldConvert: true,
+            },
+            // Basic negation cases
+            {
+                actual: '[$path=/^((?!a).)*$/]example.com##.foo',
+                expected: [
+                    'example.com##:not(:matches-path(/a/)) .foo',
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$path=/^((?!\\/page).)*$/]example.com##.foo',
+                expected: [
+                    String.raw`example.com##:not(:matches-path(/\\/page/)) .foo`,
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$path=/page]ya.ru##p',
+                expected: [
+                    'ya.ru##:matches-path(/page) p',
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$path=/page]ya.ru#@#p',
+                expected: [
+                    'ya.ru#@#:matches-path(/page) p',
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$path=/\\/(sub1|sub2)\\/page\\.html/]ya.ru##p',
+                expected: [
+                    String.raw`ya.ru##:matches-path(/\\/(sub1|sub2)\\/page\\.html/) p`,
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$path=/^((?!\\/(sub1|sub2)\\/page\\.html).)*$/]ya.ru##p',
+                expected: [
+                    String.raw`ya.ru##:not(:matches-path(/\\/(sub1|sub2)\\/page\\.html/)) p`,
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$path=/^((?!\\/page).)*$/]ya.ru##p',
+                expected: [
+                    String.raw`ya.ru##:not(:matches-path(/\\/page/)) p`,
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$path=/sexykpopidol]blog.livedoor.jp###containerWrap > #container > .blog-title-outer + #content.hfeed',
+                expected: [
+                    'blog.livedoor.jp##:matches-path(/sexykpopidol) #containerWrap > #container > .blog-title-outer + #content.hfeed',
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$path=/^((?!\\/search\\?q=.*?tbm=shop).)*$/]www.google.*###test',
+                expected: [
+                    String.raw`www.google.*##:not(:matches-path(/\\/search\\?q=.*?tbm=shop/)) #test`,
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$path=/\\/[a|b|,]\\/page\\.html/]example.com###test',
+                expected: [
+                    String.raw`example.com##:matches-path(/\/[a|b|):]\/page\.html/ #test`,
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$path=/^((?!\\/page).)*$/]example.com#@$#h1 { background-color: blue !important }',
+                expected: [
+                    String.raw`example.com#@$#:not(:matches-path(/\\/page/)) h1:style(background-color: blue !important)`,
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: String.raw`[$path=/\/(sub1|sub2)\/page\.html/]example.org#$?#p:contains(/[\w\W]{30,}/) { background: #ff0033 !important; }`,
+                expected: [
+                    String.raw`example.org#$?#:matches-path(/\\/(sub1|sub2)\\/page\\.html/) p:contains(/[\w\W]{30,}/):style(background: #ff0033 !important;)`,
+                ],
+                shouldConvert: true,
+            },
+            // [$domain=....] modifier
+            {
+                actual: '[$domain=example.com]##.textad',
+                expected: [
+                    'example.com##.textad',
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$domain=example.com]#@#.textad',
+                expected: [
+                    'example.com#@#.textad',
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$domain=example.com|example.org]###adblock',
+                expected: [
+                    'example.com,example.org###adblock',
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$domain=example.com|example.org]#@##adblock',
+                expected: [
+                    'example.com,example.org#@##adblock',
+                ],
+                shouldConvert: true,
+            },
+            // PIPE separator escaped for now because of issue with parser
+            // https://github.com/AdguardTeam/tsurlfilter/issues/121
+            {
+                actual: '[$domain=/example.(com\\|org)/]##.banner',
+                expected: [
+                    '/example.(com\\|org)/##.banner',
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$domain=/example.(com\\|org)/]##.banner',
+                expected: [
+                    '/example.(com\\|org)/##.banner',
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$domain=/example.(com\\|org)/|foo.com]##.banner',
+                expected: [
+                    '/example.(com\\|org)/,foo.com##.banner',
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$domain=/example.(com\\|org)/|foo.com]#@#.banner',
+                expected: [
+                    '/example.(com\\|org)/,foo.com#@#.banner',
+                ],
+                shouldConvert: true,
+            },
+            // [$url=....] modifier
+            {
+                actual: '[$url=||example.com/content/*]##div.textad',
+                expected: [
+                    '/^(http|https|ws|wss)://([a-z0-9-_.]+\\.)?example\\.com\\/content\\/.*/##div.textad',
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$url=|example.org]#@#h1',
+                expected: [
+                    '/^example\\.org/#@#h1',
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$url=|example.org]#@#h1',
+                expected: [
+                    '/^example\\.org/#@#h1',
+                ],
+                shouldConvert: true,
+            },
+            {
+                actual: '[$url=|example.org|]#@#h1',
+                expected: [
+                    '/^example\\.org$/#@#h1',
+                ],
+                shouldConvert: true,
+            },
+        ])('should convert \'$actual\' to \'$expected\'', (testData) => {
+            expect(testData).toBeConvertedProperly(CosmeticRuleConverter, 'convertToUbo');
+        });
+    });
 });
