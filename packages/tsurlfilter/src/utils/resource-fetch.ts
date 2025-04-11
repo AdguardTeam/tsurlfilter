1f737f42c0b5823573434a9508f9b7cca436edab
scripthunter7 <d.tota@adguard.com>
2025-04-11 18:38:13 +0200

    improve code

--- packages/tsurlfilter/src/utils/resource-fetch.ts
+++ packages/tsurlfilter/src/utils/resource-fetch.ts
@@ -2,6 +2,9 @@ import type { ByteRange } from './byte-range';
 
 /**
  * Minimum length of the byte range in bytes.
+ *
+ * We need this value to keep a minimal byte range when fetching a resource.
+ * It is needed to temporarily fix https://issues.chromium.org/issues/405286894#comment12.
  */
 const MIN_RANGE_LENGTH = 4096;
 
@@ -23,38 +26,37 @@ export const fetchExtensionResourceText = async (
     range?: ByteRange,
     minRangeLength: number = MIN_RANGE_LENGTH,
 ): Promise<string> => {
-    if (range) {
-        const rangeLength = range.end - range.start + 1;
-        const end = rangeLength < minRangeLength
-            ? range.start + minRangeLength
-            : range.end;
-
-        const headers = {
-            Range: `bytes=${range.start}-${end}`,
-        };
-
-        const response = await fetch(url, { headers });
-
-        if (end !== range.end) {
-            const buffer = await response.arrayBuffer();
-            const slicedBuffer = buffer.slice(0, rangeLength);
-            const slicedResponse = new Response(slicedBuffer, {
-                headers: response.headers,
-                status: response.status,
-                statusText: response.statusText,
-            });
-
-            const text = await slicedResponse.text();
-
-            return text;
-        }
-
+    if (!range) {
+        const response = await fetch(url);
         const text = await response.text();
         return text;
     }
 
-    const response = await fetch(url);
-    const text = await response.text();
+    const rangeLength = range.end - range.start + 1;
+    const end = rangeLength < minRangeLength
+        ? range.start + minRangeLength
+        : range.end;
 
+    const headers = {
+        Range: `bytes=${range.start}-${end}`,
+    };
+
+    const response = await fetch(url, { headers });
+
+    if (end !== range.end) {
+        const buffer = await response.arrayBuffer();
+        const slicedBuffer = buffer.slice(0, rangeLength);
+        const slicedResponse = new Response(slicedBuffer, {
+            headers: response.headers,
+            status: response.status,
+            statusText: response.statusText,
+        });
+
+        const text = await slicedResponse.text();
+
+        return text;
+    }
+
+    const text = await response.text();
     return text;
 };
