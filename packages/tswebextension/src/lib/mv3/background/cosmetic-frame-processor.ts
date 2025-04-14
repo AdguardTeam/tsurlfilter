1e44324e83d81a954751fa4a4e06b838f87957cc
Dmitry Seregin <d.seregin@adguard.com>
2025-04-14 14:25:58 +0300

    AG-39829: using userScripts.execute for inject not registered scripts && AG-41183
    Squashed commit of the following:
    
    commit cc2e982909c5b6fe6b6d8ee5ddbb66c0a7c7a8b8
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Mon Apr 14 13:42:56 2025 +0300
    
        Revert "archive for test"
        
        This reverts commit a2f7fb3381a15059680459bb1294c60f6500bf99.
    
    commit a2f7fb3381a15059680459bb1294c60f6500bf99
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Mon Apr 14 13:42:49 2025 +0300
    
        archive for test
    
    commit ed21b7ce5c46ad9a3b9948f6bb8bfa0cf7c2f1ea
    Merge: aeeb923dd 703ecdae8
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Fri Apr 11 14:45:22 2025 +0300
    
        Merge branch 'release/browser-extension-v5.2' into feature/AG-39829-3
    
    commit aeeb923ddae031271151cc916e7b295d112ccd39
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Fri Apr 11 13:02:27 2025 +0300
    
        very important fixes
    
    commit 4d93b56e3107a603f97adc0cbaa20b5bf137a707
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Fri Apr 11 01:17:39 2025 +0300
    
        fixed linter
    
    commit 1044d51f9084f1eb3570c7d3d225574575473300
    Merge: eb34d583e 98b356a57
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Fri Apr 11 00:20:05 2025 +0300
    
        Merge branch 'release/browser-extension-v5.2' into feature/AG-39829
    
    commit eb34d583efbd8ed908041dc920abfbb018c60138
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Fri Apr 11 00:17:11 2025 +0300
    
        fixed changelog
    
    commit d77024586855633207cdb396790da170ce7ba190
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Fri Apr 11 00:09:30 2025 +0300
    
        Revert "for test"
        
        This reverts commit 60083bcc9c146b6e6de4d736dc885e711a400810.
    
    commit 60083bcc9c146b6e6de4d736dc885e711a400810
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Fri Apr 11 00:08:57 2025 +0300
    
        for test
    
    commit 36014385111109771a0799f3775f678a2562ce55
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Thu Apr 10 23:59:45 2025 +0300
    
        AG-41183: fixed not logged scriptlets
    
    commit f3345e82d91dc6f21eb4ee982daf1d291a041707
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Thu Apr 10 23:21:56 2025 +0300
    
        Revert "for tests"
        
        This reverts commit 129c654fc2ba2ec9bf361ea26fbbdcf9673970dd.
    
    commit 129c654fc2ba2ec9bf361ea26fbbdcf9673970dd
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Thu Apr 10 23:21:50 2025 +0300
    
        for tests
    
    commit 248ae5163521b4b895fda6d93158b2a4f961f637
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Thu Apr 10 23:12:27 2025 +0300
    
        exclude multiple injection
    
    commit 3fb505a6ec4197447463e6eabcc83f53005d5c58
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Fri Apr 4 20:00:07 2025 +0300
    
        added links to descriptions
    
    commit 20261a99dd23e7ad22504ea6fc07bd7a51a9b626
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Wed Apr 2 22:50:58 2025 +0300
    
        Revert "archive for tests"
        
        This reverts commit 58d975f9a0c3442ac4a8f43342c3ec64a4f2b851.
    
    commit 58d975f9a0c3442ac4a8f43342c3ec64a4f2b851
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Wed Apr 2 22:50:47 2025 +0300
    
        archive for tests
    
    commit 1ad06aa3a55dad8f7901a5dc7f05fee39a376671
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Wed Apr 2 01:19:57 2025 +0300
    
        better CWS comments
    
    commit 805b77ad7d9be5411ebca604a39b2831c09b18b6
    Merge: 68e2fd2f1 245018336
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Mon Mar 31 22:21:57 2025 +0300
    
        Merge branch 'release/browser-extension-v5.2' into feature/AG-39829
    
    commit 68e2fd2f1694994a34aaaab7e71f6a417924f906
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Fri Mar 14 13:52:15 2025 +0300
    
        changed fixme
    
    commit 404c3b1fd78d200a93dfe0bdf2d948f906d91d28
    Author: Dmitriy Seregin <d.seregin@adguard.com>
    Date:   Thu Mar 13 16:52:30 2025 +0300
    
        Revert "archive for tests"
        
        This reverts commit d02a1a12e319111f2a1856da4bc52f1a75153aaa.
    
    ... and 33 more commits

--- packages/tswebextension/src/lib/mv3/background/cosmetic-frame-processor.ts
+++ packages/tswebextension/src/lib/mv3/background/cosmetic-frame-processor.ts
@@ -15,6 +15,7 @@ import { appContext } from './app-context';
 import { DocumentApi } from './document-api';
 import { engineApi } from './engine-api';
 import { CosmeticApi } from './cosmetic-api';
+import { UserScriptsApi } from './user-scripts-api';
 
 /**
  * Cosmetic frame processor.
@@ -177,25 +178,43 @@ export class CosmeticFrameProcessor {
 
         const cosmeticResult = engineApi.getCosmeticResult(url, matchingResult.getCosmeticOption());
 
-        const {
-            scriptTexts,
-            scriptletDataList,
-        } = CosmeticApi.getScriptsAndScriptletsData(cosmeticResult);
-
         const { configuration } = appContext;
         const areHitsStatsCollected = configuration?.settings.collectStats || false;
 
         const cssText = CosmeticApi.getCssText(cosmeticResult, areHitsStatsCollected);
 
-        tabsApi.updateFrameContext(tabId, frameId, {
-            matchingResult,
-            cosmeticResult,
-            preparedCosmeticResult: {
+        const partialFrameContext: Partial<FrameMV3> = { matchingResult, cosmeticResult };
+
+        /**
+         * If user scripts API is supported, we should store one combined script
+         * text, because it will be injected once and it is more efficient.
+         */
+        if (UserScriptsApi.isSupported) {
+            const scriptText = CosmeticApi.getScriptText(cosmeticResult.getScriptRules());
+
+            partialFrameContext.preparedCosmeticResult = {
+                cssText,
+                scriptText,
+            };
+        } else {
+            /**
+             * Otherwise, we should store separate script texts and scriptlet
+             * data, because they will be injected separately with different
+             * params.
+             */
+            const {
                 scriptTexts,
                 scriptletDataList,
+            } = CosmeticApi.getScriptsAndScriptletsData(cosmeticResult);
+
+            partialFrameContext.preparedCosmeticResult = {
                 cssText,
-            },
-        });
+                scriptTexts,
+                scriptletDataList,
+            };
+        }
+
+        tabsApi.updateFrameContext(tabId, frameId, partialFrameContext);
     }
 
     /**
