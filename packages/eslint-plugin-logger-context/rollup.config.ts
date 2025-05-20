93fcdd419b46e4725932a601382edc11a1067687
Dmitriy Seregin <d.seregin@adguard.com>
2025-05-20 16:37:53 +0300

    added todo

--- packages/eslint-plugin-logger-context/rollup.config.ts
+++ packages/eslint-plugin-logger-context/rollup.config.ts
@@ -3,6 +3,9 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 
+// TODO: commonjs format is needed since we use eslint in common js format and
+// it require to load plugins in cjs format, when we will migrate to eslint9 or
+// another linter we can remove this format.
 export default {
     input: 'src/index.ts',
     output: [{
