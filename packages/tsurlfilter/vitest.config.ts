400baffa92c4fe81e770fe3945005bc4ce61e406
Slava Leleka <v.leleka@adguard.com>
2025-05-23 14:13:40 -0400

    chore: exclude .rollup.cache from test coverage reports for tsurlfilter

--- packages/tsurlfilter/vitest.config.ts
+++ packages/tsurlfilter/vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         watch: false,
+        coverage: {
+            exclude: [
+                '.rollup.cache',
+            ],
+        },
     },
 });
