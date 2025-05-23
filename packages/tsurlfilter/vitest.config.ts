8d40f26225a8e179ebcad15e2a5293c58ed984b5
Slava Leleka <v.leleka@adguard.com>
2025-05-23 14:28:49 -0400

    fix exclude

--- packages/tsurlfilter/vitest.config.ts
+++ packages/tsurlfilter/vitest.config.ts
@@ -3,10 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         watch: false,
-        coverage: {
-            exclude: [
-                '.rollup.cache/**',
-            ],
-        },
+        exclude: [
+            '.rollup.cache/**',
+        ],
     },
 });
