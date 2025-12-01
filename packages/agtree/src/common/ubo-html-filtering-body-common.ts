344d405d8b54c08f4d3af8087848b6ed861fe411
Kurbanali Ruslan <r.kurbanali@adguard.com>
2025-12-01 18:42:42 +0500

    fixed util function

--- packages/agtree/src/common/ubo-html-filtering-body-common.ts
+++ packages/agtree/src/common/ubo-html-filtering-body-common.ts
@@ -13,15 +13,15 @@ import { UBO_RESPONSEHEADER_FN } from '../utils/constants';
  */
 export function isUboResponseHeaderRemovalRuleBody(node: HtmlFilteringRuleBody): boolean {
     return (
+        // Must have exactly one selector list
+        node.children.length === 1
         // Must have exactly one selector
-        node.selectors.length === 1
-        // Selector must not have tag name
-        && !node.selectors[0].tagName
-        // Selector must not have attributes
-        && node.selectors[0].attributes.length === 0
-        // Selector must have exactly one pseudo-class
-        && node.selectors[0].pseudoClasses.length === 1
-        // Pseudo-class must be `responseheader`
-        && node.selectors[0].pseudoClasses[0].name.value === UBO_RESPONSEHEADER_FN
+        && node.children[0].children.length === 1
+        // Must have exactly one part
+        && node.children[0].children[0].children.length === 1
+        // Should be a pseudo class
+        && node.children[0].children[0].children[0].type === 'HtmlFilteringRuleSelectorPseudoClass'
+        // Pseudo class name should match `responseheader`
+        && node.children[0].children[0].children[0].name.value === UBO_RESPONSEHEADER_FN
     );
 }
