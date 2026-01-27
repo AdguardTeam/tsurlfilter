ba2673770032394c2efa242722599436cf371865
Maxim Topciu <mtopciu@adguard.com>
2026-01-27 07:08:15 +0000

    Pull request 1498: AG-50414 compare denyallow domains when matching badfilter rules
    Squashed commit of the following:
    
    commit b802477f404e68e86f4339c13cd58e7cbfe5f1ff
    Author: Maxim Topciu <mtopciu@adguard.com>
    Date:   Mon Jan 26 15:13:17 2026 +0200
    
        AG-50414 compare denyallow domains when matching badfilter rules

--- packages/tsurlfilter/src/rules/network-rule.ts
+++ packages/tsurlfilter/src/rules/network-rule.ts
@@ -1428,6 +1428,10 @@ export class NetworkRule implements IRule {
             return false;
         }
 
+        if (!stringArraysEquals(this.getDenyAllowDomains(), specifiedRule.getDenyAllowDomains())) {
+            return false;
+        }
+
         return true;
     }
 
