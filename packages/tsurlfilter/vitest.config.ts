4000958fbfaaa7753a8b2bf3be48b6679c0df396
scripthunter7 <d.tota@adguard.com>
2026-04-17 17:05:31 +0300

    Increase timeout

--- packages/tsurlfilter/vitest.config.ts
+++ packages/tsurlfilter/vitest.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
-        testTimeout: 30000,
+        // FIXME
+        testTimeout: 40000,
         setupFiles: [
             './test/setup/index.ts',
             './test/setup/custom-matchers/index.ts',
