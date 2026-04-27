bf1d4effab851067aea709cf9cfec9350a61a922
Sergei Belov <s.belov@adguard.com>
2026-04-27 16:13:03 +0500

    AG-53294 add type check for getRemoveParamUrl method in Engine

--- packages/tsurlfilter/test/builders/rollup-ts/index.test-d.ts
+++ packages/tsurlfilter/test/builders/rollup-ts/index.test-d.ts
@@ -3,5 +3,6 @@ import { expectNotType } from 'tsd';
 import { Engine } from '@adguard/tsurlfilter';
 
 expectNotType<any>(Engine);
+expectNotType<any>(Engine.prototype.getRemoveParamUrl);
 
 console.log('Smoke test passed in index.test-d.ts');
